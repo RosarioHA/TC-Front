33d913ef85224ab52fafa6c5238acce3d4086923
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-10-30 13:39:32 -0300

    crea estilos de botones basicos

--- src/views/landing.jsx
+++ src/views/landing.jsx
@@ -3,6 +3,14 @@ const Landing = () => {
     <div>
       <div>
         <p>LANDING: DASHBOARD RESUMEN</p>
+        <div className="d-flex flex-column col-6">
+          <button className="btn-primario-s mb-2">Primario S</button>
+          <button className="btn-primario-l mb-2">Primario L</button>
+          <button className="btn-secundario-s mb-2">Secundario S</button>
+          <button className="btn-secundario-l mb-2">Secundario L</button>
+          <button className="btn-terciario mb-2">Terciario</button>
+          <button className="btn-danger mb-2">Danger</button>
+        </div>
       </div>
     </div>
     );
