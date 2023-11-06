4e947fc1d133c514b1c8754ee28f6684638781fb
vecarope <vecarope@mac-vcaro.local>
2023-11-06 12:55:46 -0300

    Add sidebar and Navbar

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,9 +1,15 @@
-const Navbar = () => {
+const Navbar = () =>
+{
   return (
-    <div>
-      <p>NAVBAR</p>
-    </div>
-    );
+    <nav className="d-flex  bg-white border-bottom justify-content-end me-5 w-100 sticky-top mt-1 py-3">
+      <div className="align-self-center mx-2 text-sans-h5">
+        <span>Hola, $userName</span>
+        </div>
+        <button className="btn-logout mx-2 my-2" type="button" onClick=''><u>Cerrar Sesión</u><i className="material-symbols-outlined">
+          logout
+        </i></button> 
+    </nav>
+  );
 };
 
 export default Navbar;
