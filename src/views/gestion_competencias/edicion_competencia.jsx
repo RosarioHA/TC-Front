73f9df17abeacd739308bedab5acf1b17d9e81f9
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-11-27 16:26:56 -0300

    estructura vistas editar usuario y editar competencia

--- src/views/gestion_competencias/edicion_competencia.jsx
+++ src/views/gestion_competencias/edicion_competencia.jsx
@@ -1,11 +1,69 @@
-const EdicionCompetencia = () =>
-{
+import { useNavigate } from "react-router-dom";
+
+const EdicionCompetencia = () => {
+  const history = useNavigate();
+
+  const handleBackButtonClick = () => {
+    history(-1);
+  };
+
   return (
-    <>
-      <p>VISUALIZAR Y EDITAR COMPETENCIA</p>
-      <p>al entrar a la vista se muestra un modo visualizacion de la informacion de la competencia.<br/>
-       Luego, al dar click en un boton Editar, cambia a campos editables.</p>
-    </>
+    <div className="container col-10 my-4">
+      <h2 className="text-sans-h2 mb-3">Gestión de Competencias</h2>
+      <div className="d-flex  align-items-center justify-content-between mb-5">
+        <div className="d-flex align-items-center">
+          <button className="btn-secundario-s" onClick={handleBackButtonClick}>
+            <i className="material-symbols-rounded me-2">arrow_back_ios</i>
+            <p className="mb-0">Volver</p>
+          </button>
+          <h3 className="text-sans-h3 ms-3 mb-0">Competencia: $NombreCompetencia</h3>
+        </div>
+        <button className="btn-secundario-s">
+          <i className="material-symbols-rounded me-2">edit</i>
+          <p className="mb-0">Editar</p>
+        </button> 
+      </div>
+
+      <form>
+        <div className="mb-4">
+          NOMBRE
+        </div>
+
+        <div className="mb-4">
+          REGION
+        </div>
+
+        <div className="mb-4">
+          SECTOR
+        </div>
+
+        <div className="mb-4">
+          AMBITO
+        </div>
+
+        <div className="mb-4">
+          ORIGEN
+        </div>
+
+        <div className="mb-4">
+          USUARIOS
+        </div>
+
+        <div className="mb-4">
+          OFICIO - puede subir otro archivo? solo puede descargar?
+        </div>
+
+        <div className="mb-4">
+          PLAZO
+        </div>
+
+        <button className="btn-primario-s mb-5" type="submit">
+          <i className="material-symbols-rounded me-2">save</i>
+          <p className="mb-0">Guardar</p>
+        </button>
+
+      </form>
+    </div>
   );
 }
 
--- src/views/gestion_usuarios/edicion_usuario.jsx
+++ src/views/gestion_usuarios/edicion_usuario.jsx
@@ -1,11 +1,61 @@
-const EdicionUsuario = () =>
-{
+import { useNavigate } from "react-router-dom";
+
+const EdicionUsuario = () => {
+  const history = useNavigate();
+
+  const handleBackButtonClick = () => {
+    history(-1);
+  };
+
   return (
-    <>
-      <p>VISUALIZAR Y EDITAR USUARIO</p>
-      <p>al entrar a la vista se muestra un modo visualizacion de la informacion del usuario.<br/>
-       Luego, al dar click en un boton Editar, cambia a campos editables.</p>
-    </>
+    <div className="container col-10 my-4">
+      <h2 className="text-sans-h2 mb-3">Administrar Usuarios</h2>
+      <div className="d-flex  align-items-center justify-content-between mb-5">
+        <div className="d-flex align-items-center">
+          <button className="btn-secundario-s" onClick={handleBackButtonClick}>
+            <i className="material-symbols-rounded me-2">arrow_back_ios</i>
+            <p className="mb-0">Volver</p>
+          </button>
+          <h3 className="text-sans-h3 ms-3 mb-0">Perfil de: $NombreUsuario</h3>
+        </div>
+        <button className="btn-secundario-s">
+          <i className="material-symbols-rounded me-2">edit</i>
+          <p className="mb-0">Editar</p>
+        </button> 
+      </div>
+
+      <form>
+        <div className="mb-4">
+          RUT
+        </div>
+
+        <div className="mb-4">
+          NOMBRE
+        </div>
+
+        <div className="mb-4">
+          CORREO
+        </div>
+
+        <div className="mb-4">
+          PERFIL
+        </div>
+
+        <div className="mb-4">
+          ESTADO
+        </div>
+
+        <div className="mb-4">
+          COMPETENCIAS
+        </div>
+
+        <button className="btn-primario-s mb-5" type="submit">
+          <i className="material-symbols-rounded me-2">save</i>
+          <p className="mb-0">Guardar</p>
+        </button>
+
+      </form>
+    </div>
   );
 }
 
