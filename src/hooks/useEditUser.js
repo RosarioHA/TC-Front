37c43687f8f5844de3a3418cd90fe30bce327c00
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-12-06 14:37:18 -0300

    cambia endpoint de hook useEditUser.js

--- src/hooks/useEditUser.js
+++ src/hooks/useEditUser.js
@@ -6,15 +6,12 @@ export const useEditUser = () => {
   const [error, setError] = useState(null);
 
   const editUser = async (userId, userData) => {
-    console.log('URL:', `/users/${userId}/`);
-    console.log('Data:', userData);
+    console.log('Data before formatting:', userData);
     setIsLoading(true);
     setError(null);
 
     try {
-      // Asegúrate de que userData contenga todas las claves necesarias
       const formattedData = {
-        // Asegúrate de incluir todas las claves necesarias aquí
         nombre_completo: userData.nombre_completo,
         rut: userData.rut,
         email: userData.email,
@@ -22,10 +19,11 @@ export const useEditUser = () => {
         sector: userData.sector,
         region: userData.region,
         is_active: userData.is_active,
-        // ... otras claves según sea necesario
       };
 
-      const response = await apiTransferenciaCompentencia.patch(`users/${userId}/`, formattedData);
+      console.log('Data after formatting:', formattedData);
+
+      const response = await apiTransferenciaCompentencia.patch(`users/update_profile/`, formattedData);
       return response.data;
     } catch (error) {
       setError(error);
