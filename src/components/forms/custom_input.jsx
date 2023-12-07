1d717306f29998b9e8768442aa046c3f21d67b4a
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-12-07 14:51:35 -0300

    restaura componente CustomInput eliminado por error

--- src/components/forms/custom_input.jsx
+++ src/components/forms/custom_input.jsx
@@ -1,74 +1,71 @@
-import { useState, useRef, useEffect, forwardRef } from 'react';
+import { useState, forwardRef } from "react";
 
-const DropdownSelect = forwardRef(
-  ({ label, placeholder, options, onSelectionChange, readOnly, initialValue }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedOption, setSelectedOption] = useState(initialValue || null);
-    const dropdownRef = useRef(null);
+const CustomInput = forwardRef (
+  ({ label, placeholder, id, maxLength, error, readOnly, onChange, initialValue, ...props }, ref) => {
+    const [inputValue, setInputValue] = useState(initialValue || '');
 
-    useEffect(() => {
-      function handleClickOutside(event) {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-          setIsOpen(false);
-        }
-      }
-
-      if (isOpen) {
-        document.addEventListener('mousedown', handleClickOutside);
+    const handleInputChange = (e) => {
+      const value = e.target.value;
+      if (maxLength !== null && maxLength !== undefined && value.length > maxLength) {
+        setInputValue(value.slice(0, maxLength));
       } else {
-        document.removeEventListener('mousedown', handleClickOutside);
+        setInputValue(value);
       }
 
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside);
-      };
-    }, [isOpen]);
-
-    const toggleDropdown = () => {
-      setIsOpen(!isOpen);
-    };
-
-    const handleOptionClick = (option) => {
-      setSelectedOption(option);
-      setIsOpen(false);
       // Llamar a la función de cambio directamente en el componente padre
-      if (onSelectionChange) {
-        onSelectionChange(option);
+      if (onChange) {
+        onChange(value);
       }
     };
+
+    const counterClass = inputValue.length === maxLength ? "text-sans-h6-darkred" : "text-sans-h6";
 
     return (
-      <div className={`input-container ${readOnly ? 'readonly' : ''}`}>
-        <label className="text-sans-h5 input-label">{label}</label>
-        <button
-          type="button"
-          onClick={toggleDropdown}
-          className={`text-sans-p dropdown-btn ${readOnly ? 'disabled' : ''}`}
-        >
-          {selectedOption ? selectedOption.label : placeholder}
-          {!readOnly && (
-            <i className="material-symbols-rounded ms-2">{isOpen ? 'expand_less' : 'expand_more'}</i>
-          )}
-        </button>
-        {isOpen && !readOnly && (
-          <div className="dropdown d-flex flex-column" ref={dropdownRef}>
-            {options.map((option, index) => (
-              <div
-                key={index}
-                onClick={() => handleOptionClick(option)}
-                className={`p-3 ${
-                  option.value === selectedOption?.value ? 'selected-option' : 'unselected-option'
-                }`}
-              >
-                {option.label}
+      <div className="d-flex flex-column input-container">
+        {readOnly ? (
+          <>
+            <label className="text-sans-h5 input-label ms-3 ms-sm-0">{label}</label>
+            <input
+              className={`input-s p-3 text-sans-p ${error ? 'input-error' : ''}`}
+              type="text"
+              placeholder={placeholder}
+              id={id}
+              value={inputValue}
+              onChange={handleInputChange}
+              ref={ref}
+              disabled
+              {...props}
+            />
+          </>
+        ) : (
+          <>
+            <label className="text-sans-h5 input-label ms-3 ms-sm-0">{label}</label>
+            <input
+              className={`input-s p-3 text-sans-p ${error ? 'input-error' : ''}`}
+              type="text"
+              placeholder={placeholder}
+              id={id}
+              value={inputValue}
+              onChange={handleInputChange}
+              ref={ref}
+              {...props}
+            />
+            {maxLength !== null && maxLength !== undefined && (
+              <div className="d-flex justify-content-end mt-1">
+                <p className={counterClass}>
+                  {inputValue.length}/{maxLength} caracteres.
+                </p>
               </div>
-            ))}
-          </div>
+            )}
+            {error && (
+              <p className="text-sans-h6-darkred mt-2 mb-0">{error}</p>
+            )}
+          </>
         )}
       </div>
     );
   }
 );
 
-DropdownSelect.displayName = 'DropdownSelect';
-export default DropdownSelect;
+CustomInput.displayName = 'CustomInput';
+export default CustomInput;
