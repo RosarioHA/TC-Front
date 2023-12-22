bde24b38200a191d2d1e1b33022a9cd31588dbbb
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-12-22 16:50:38 -0300

    estilos componente Personal

--- src/components/tables/Personal.jsx
+++ src/components/tables/Personal.jsx
@@ -3,7 +3,6 @@ import DropdownSelect from "../dropdown/select";
 
 const Personal = () => {
   const [personas, setPersonas] = useState([{ id: 1 }]);
-  
 
   const agregarPersona = () => {
     const nuevaPersona = { id: personas.length + 1 };
@@ -16,78 +15,69 @@ const Personal = () => {
     setPersonas(personasActualizados);
   };
 
-  
-
   return (
-      <div className="my-4">
-        <div className="col my-4">
-          <div className="row">
-            <div className="col-1">
-              <p className="text-sans-p-bold mt-3">Estamento</p>
-            </div>
-            <div className="col-2">
-              <DropdownSelect 
-                placeholder="Estamento"
-                options=""/>
-            </div> 
-            <div className="col d-flex justify-content-end">
-              {/* <button
-                className="btn-terciario-ghost mt-3"
-                onClick={() => eliminarEstamento(estamento.id)}
-              >
-                <i className="material-symbols-rounded me-2">delete</i>
-                <p className="mb-0 text-decoration-underline">Borrar estamento</p>
-              </button> */}
-            </div>
+    <div className="my-4">
+      <div className="col my-4">
+        <div className="row">
+          <div className="col-1">
+            <p className="text-sans-p-bold mt-3">Estamento</p>
           </div>
-
-          {/* aparece dinamicamente */}
-          <div className="row">
-            <p className="text-sans-p mt-4">Luego agrega los profesionales que correspondan a este estamento:</p>
-          </div>
-
-          <div className="row">
-            <div className="col-1"> <p className="text-sans-p-bold mt-3">N°</p> </div>
-            <div className="col"> <p className="text-sans-p-bold mt-3">Calidad jurídica</p> </div>
-            <div className="col"> <p className="text-sans-p-bold mt-3">Renta bruta mensual</p> </div>
-            <div className="col"> <p className="text-sans-p-bold mt-3">Grado <br/> (Si corresponde)</p> </div>
-            <div className="col"> <p className="text-sans-p-bold mt-3">Acción</p> </div>
-          </div>
-          {personas.map((persona) => (
-            <div key={persona.id} className="row my-3">
-              <div className="col-1"> <p className="text-sans-p-bold mt-3">1</p> </div>
-              <div className="col">
-                <DropdownSelect 
-                placeholder="Calidad jurídica"
-                options=""/>
-              </div>
-              {/* ajustar segun estilos creados por Vero para campos de input en 5.2 */}
-              <div className="col pt-3"> <input></input> </div>
-              <div className="col pt-3"> <input></input> </div>
-              <div className="col">
-                {personas.length > 1 && (
-                <button
-                  className="btn-terciario-ghost"
-                  onClick={() => eliminarPersona(persona.id)}
-                >
-                  <i className="material-symbols-rounded me-2">delete</i>
-                  <p className="mb-0 text-decoration-underline">Borrar</p>
-                </button> 
-                )}
-              </div>
-            </div>
-          
-          ))}
+          <div className="col-2">
+            <DropdownSelect 
+              placeholder="Estamento"
+              options=""/>
+          </div> 
         </div>
 
-        <button
-          className="btn-secundario-s m-2"
-          onClick={agregarPersona}
-          >
-            <i className="material-symbols-rounded me-2">add</i>
-            <p className="mb-0 text-decoration-underline">Agregar $estamentoElegido</p>
-        </button>
+        {/* aparece dinamicamente */}
+        <div className="row">
+          <p className="text-sans-p mt-4">Luego agrega los profesionales que correspondan a este estamento:</p>
+        </div>
+
+        <div className="row">
+          <div className="col-1"> <p className="text-sans-p-bold mt-3">N°</p> </div>
+          <div className="col"> <p className="text-sans-p-bold mt-3">Calidad jurídica</p> </div>
+          <div className="col"> <p className="text-sans-p-bold mt-3">Renta bruta mensual</p> </div>
+          <div className="col"> <p className="text-sans-p-bold mt-3">Grado <br/> (Si corresponde)</p> </div>
+          <div className="col"> <p className="text-sans-p-bold mt-3">Acción</p> </div>
+        </div>
+
+        {personas.map((persona) => (
+          <div 
+          key={persona.id} 
+          className={`row py-3 ${persona.id % 2 === 0 ? 'white-line' : 'neutral-line'} align-items-center me-3`}>
+            <div className="col-1"> <p className="text-sans-p-bold mt-3">{persona.id}</p> </div>
+            <div className="col">
+              <DropdownSelect 
+              placeholder="Calidad jurídica"
+              options=""/>
+            </div>
+            {/* ajustar segun estilos creados por Vero para campos de input en 5.2 */}
+            <div className="col pt-3"> <input></input> </div>
+            <div className="col pt-3"> <input></input> </div>
+            <div className="col">
+              {personas.length > 1 && (
+              <button
+                className="btn-terciario-ghost"
+                onClick={() => eliminarPersona(persona.id)}
+              >
+                <i className="material-symbols-rounded me-2">delete</i>
+                <p className="mb-0 text-decoration-underline">Borrar</p>
+              </button> 
+              )}
+            </div>
+          </div>
+        ))}
       </div>
+
+      <button
+        className="btn-secundario-s m-2"
+        onClick={agregarPersona}
+      >
+        <i className="material-symbols-rounded me-2">add</i>
+        <p className="mb-0 text-decoration-underline">Agregar $estamentoElegido</p>
+      </button>
+    </div>
   )
 }
 
