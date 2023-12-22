2c8f30e0e2679303fc4f45256070c010539e63b0
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-12-22 14:37:02 -0300

    ajusta estilos de componente Costos

--- src/components/formSectorial/paso5/p5.1.jsx
+++ src/components/formSectorial/paso5/p5.1.jsx
@@ -1,20 +1,8 @@
-import { useState } from "react";
+
 import Costos from "../../tables/Costos";
 import SumatoriaCostos from "../../tables/SumatoriaCostos";
 
 const Subpaso_CincoPuntoUno = () => {
-  const [costosDirectos, setCostoDirecto] = useState([{ id: 1 }]);
-  const [costosIndirectos, setCostoIndirecto] = useState([{ id: 1 }]);
-
-  const agregarCostoDirecto = () => {
-    const nuevoIndicador = { id: costosDirectos.length + 1 };
-    setCostoDirecto([...costosDirectos, nuevoIndicador]);
-  };
-
-  const agregarCostoIndirecto = () => {
-    const nuevoIndicador = { id: costosIndirectos.length + 1 };
-    setCostoIndirecto([...costosIndirectos, nuevoIndicador]);
-  };
 
   return (
     <div className="mt-4">
@@ -25,20 +13,11 @@ const Subpaso_CincoPuntoUno = () => {
       <h6 className="text-sans-h6-primary mt-3">Por costos directos se entenderán aquellos imputables a los procedimientos específicos de la competencia analizada, y que no corresponden a unidades de soporte transversal del Ministerio o Servicio de origen. Los costos analizados responden al año n-1, es decir, al año anterior al inicio del estudio de transferencia de competencias.</h6>
       <h6 className="text-sans-h6-primary mt-3">Llenar información para al menos un subtítulo/item es obligatorio.</h6>
 
-      {costosDirectos.map((costo) => (
-        <div key={costo.id}>
+      <div >
         <Costos />
         <hr/>
       </div>
-      ))}
-
-      <button 
-      className="btn-secundario-s m-2"
-      onClick={agregarCostoDirecto}>
-        <i className="material-symbols-rounded me-2">add</i>
-        <p className="mb-0 text-decoration-underline">Agregar indicador</p>
-      </button>
-
+    
       <div className="row mt-5">
         <p className="text-sans-p-bold mb-0 col-2">Costos Directos <br/> Totales Anual ($M)</p>
         <p className="text-sans-p-blue col">(sumResult)</p>
@@ -48,21 +27,12 @@ const Subpaso_CincoPuntoUno = () => {
       <p className="text-sans-m-semibold mt-4">b. Costos indirectos</p>
       <h6 className="text-sans-h6-primary mt-3">Por costos indirectos se entenderán aquellos que no son imputables a los procedimientos específicos de la competencia analizada, pero que, al financiar unidades de soporte transversal en el Ministerio o Servicio de origen, hacen posible el ejercicio de la competencia. Los costos analizados responden al año n-1, es decir, al año anterior al inicio de estudio de transferencia de competencias.</h6>
       <h6 className="text-sans-h6-primary mt-3">Llenar información para al menos un subtítulo/item es obligatorio.</h6>
-
-      {costosIndirectos.map((costo) => (
-        <div key={costo.id}>
+ 
+      <div>
         <Costos />
         <hr/>
       </div>
-      ))}
-
-      <button 
-      className="btn-secundario-s m-2"
-      onClick={agregarCostoIndirecto}>
-        <i className="material-symbols-rounded me-2">add</i>
-        <p className="mb-0 text-decoration-underline">Agregar indicador</p>
-      </button>
-
+      
       <div className="row mt-5">
         <p className="text-sans-p-bold mb-0 col-2">Costos Directos <br/> Totales Anual ($M)</p>
         <p className="text-sans-p-blue col">(sumResult)</p>
