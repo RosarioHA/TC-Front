3875aacaf3e30799fc44f115ac16bc1c742a397a
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2023-12-29 12:46:51 -0300

    agrega espacio observaciones subdere

--- src/views/observacionesSUBDERE/pasoUnoOS.jsx
+++ src/views/observacionesSUBDERE/pasoUnoOS.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Avance } from "../../components/tables/Avance";
 import { FormularioContext } from '../../context/FormSectorial';
 import { Subpaso_uno_OS } from '../../components/obsSUBDERE/paso1/p1.1OS';
@@ -6,9 +6,11 @@ import { Subpaso_dos_OS } from '../../components/obsSUBDERE/paso1/p1.2OS';
 import { Subpaso_tres_OS } from '../../components/obsSUBDERE/paso1/p1.3OS'; 
 import { ButtonsNavigate } from "../../components/layout/ButtonsNavigate";
 import { MonoStepers } from '../../components/stepers/MonoStepers';
+import CustomTextarea from '../../components/forms/custom_textarea';
 
 const PasoUnoOS = () => {
   const { pasoData, loadingPaso, errorPaso, updateStepNumber,data } = useContext(FormularioContext);
+  const [ observacionSubdere, setObservacionSubdere ] = useState('');
   const stepNumber = 1;
   
   useEffect(() => {
@@ -25,6 +27,10 @@ const PasoUnoOS = () => {
   const paso1Data = paso1 && paso1.length > 0 ? paso1[0] : null;
   if (!paso1Data) return <div>No hay datos disponibles para el Paso 1</div>;
 
+  const handleObservacionChange = (observacion) => {
+    setObservacionSubdere(observacion);
+  };
+
   return (
     <>
       <div className="col-1">
@@ -40,8 +46,25 @@ const PasoUnoOS = () => {
           <Subpaso_uno_OS pasoData={paso1Data} marcojuridico={marcojuridico}/>
           <Subpaso_dos_OS pasoData={paso1Data} organigrama={organigramaregional} />
           <Subpaso_tres_OS pasoData={paso1Data} />
+
+          <div className="col-11">
+            <CustomTextarea 
+            label="Observaciones (Opcional)"
+            placeholder="Escribe tus observaciones de este paso del formulario"
+            maxLength={500}
+            rows={10}
+            readOnly={false}
+            value={observacionSubdere}
+            onChange={handleObservacionChange}/>
+            <div className="d-flex mb-3 text-sans-h6-primary">
+              <i className="material-symbols-rounded me-2">info</i>
+              <h6 className="mt-1">Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo </h6>
+            </div>
+          </div>
+          
           <ButtonsNavigate step={paso1Data.numero_paso} id={data.id} />
         </div>
+        
       </div>
     </>
   );
