0e940801cc05a40524226cf44aee9b42b01045e5
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-01-02 18:20:13 -0300

    paso 2.5, pendiente visor de PDF

--- src/views/observacionesSUBDERE/pasoDosOS.jsx
+++ src/views/observacionesSUBDERE/pasoDosOS.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FormularioContext } from '../../context/FormSectorial';
 import { Avance } from "../../components/tables/Avance";
 import { ButtonsNavigate } from "../../components/layout/ButtonsNavigate";
@@ -6,11 +6,13 @@ import { Subpaso_dosPuntoUnoOS } from '../../components/obsSUBDERE/paso2/p2.1OS'
 import { Subpaso_dosPuntoDosOS } from '../../components/obsSUBDERE/paso2/p2.2OS';
 import { Subpaso_dosPuntoTresOS } from '../../components/obsSUBDERE/paso2/p2.3OS';
 import { Subpaso_dosPuntoCuatroOS } from '../../components/obsSUBDERE/paso2/p2.4OS';
-import { Subpaso_dosPuntoCinco } from "../../components/formSectorial/paso2/p2.5";
+import { Subpaso_dosPuntoCincoOS } from '../../components/obsSUBDERE/paso2/p2.5OS';
 import { MonoStepers } from "../../components/stepers/MonoStepers";
+import CustomTextarea from '../../components/forms/custom_textarea';
 
 const PasoDosOS = () => {
   const { pasoData, loadingPaso, errorPaso, updateStepNumber ,data} = useContext(FormularioContext);
+  const [ observacionSubdere, setObservacionSubdere ] = useState('');
   const stepNumber = 2;
   console.log("paso data", pasoData);
 
@@ -18,11 +20,14 @@ const PasoDosOS = () => {
     updateStepNumber(stepNumber);
   }, [ updateStepNumber, stepNumber ]);
 
+  const handleObservacionChange = (observacion) => {
+    setObservacionSubdere(observacion);
+  };
+
   if (loadingPaso) return <div>Cargando...</div>;
   if (errorPaso) return <div>Error: {errorPaso.message || "Error desconocido"}</div>;
   if (!pasoData) return <div>No hay datos disponibles para el Paso 2</div>;
 
-  // Asegúrate de acceder al primer elemento del array 'paso2'
   const paso2 = pasoData.paso2 && pasoData.paso2.length > 0 ? pasoData.paso2[ 0 ] : null;
 
   if (!paso2) return <div>No hay información de paso2 disponible</div>;
@@ -59,7 +64,24 @@ const PasoDosOS = () => {
             <Subpaso_dosPuntoCuatroOS data={dataPlataformasYSoftwares} />
           </div>
           <div className="container-fluid me-5 pe-5 my-5">
-            <Subpaso_dosPuntoCinco data={dataFlujogramaCompetencia} />
+            <Subpaso_dosPuntoCincoOS data={dataFlujogramaCompetencia} />
+          </div>
+
+          {/* Seccion observaciones SUBDERE */}
+          <div className="col-11">
+            <CustomTextarea 
+            label="Observaciones (Opcional)"
+            placeholder="Escribe tus observaciones de este paso del formulario"
+            maxLength={500}
+            rows={10}
+            readOnly={false}
+            value={observacionSubdere}
+            onChange={handleObservacionChange}
+            />
+            <div className="d-flex mb-3 text-sans-h6-primary">
+              <i className="material-symbols-rounded me-2">info</i>
+              <h6 className="mt-1">Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo Texto de apoyo </h6>
+            </div>
           </div>
 
           {/*Botones navegacion  */}
