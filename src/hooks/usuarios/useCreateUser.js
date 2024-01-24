a614aaaab7c7e258a2a5c61c55bf92988e1dbcc5
JaimeHernandezE <jhearquitecto@gmail.com>
2024-01-24 16:39:27 -0300

    FIX: validación de errores para creación de usuarios

--- src/hooks/usuarios/useCreateUser.js
+++ src/hooks/usuarios/useCreateUser.js
@@ -15,15 +15,22 @@ export const useCreateUser = () =>
     {
       const response = await apiTransferenciaCompentencia.post('users/', userData);
       return response.data;
-    } catch (error)
-    {
-      setError(error);
-      throw error;
+    } catch (error) {
+      let errorMsg = 'Ocurrió un error al crear el usuario';     
+      // Verifica si el error tiene una respuesta y extrae la información relevante
+      if (error.response && error.response.data) {
+        errorMsg = error.response.data.errors || errorMsg;
+      }
+    
+      setError({ message: errorMsg || {} });
+      throw { message: errorMsg || {} };
     } finally
     {
       setIsLoading(false);
     }
   };
+
+  console.log('Error hook:', error )
 
   return { createUser, isLoading, error };
 };
--- src/views/gestion_usuarios/creacion_usuario.jsx
+++ src/views/gestion_usuarios/creacion_usuario.jsx
@@ -7,13 +7,13 @@ import DropdownSelect from "../../components/dropdown/select";
 import DropdownSelectBuscador from "../../components/dropdown/select_buscador";
 import DropdownSinSecciones from "../../components/dropdown/checkbox_sinSecciones_conTabla";
 import { RadioButtons } from "../../components/forms/radio_btns";
-//import { competencias } from "../../Data/Competencias";
 import { esquemaCreacionUsuario } from "../../validaciones/esquemaValidacion";
 import { useCreateUser } from "../../hooks/usuarios/useCreateUser";
 import { useRegion } from "../../hooks/useRegion";
 import { useGroups } from "../../hooks/useGroups";
 import { useSector } from "../../hooks/useSector";
 import { useFiltroCompetencias } from "../../hooks/useFiltrarCompetencias";
+import { object } from "yup";
 
 const initialValues = {
   rut: '',
@@ -40,6 +40,7 @@ const CreacionUsuario = () => {
   const [ regionId, setRegionId ] = useState(null);
   const [ sectorId, setSectorId ] = useState(null);
   const { dataFiltroCompetencias, loadingFiltroCompetencias } = useFiltroCompetencias(regionId, sectorId);
+  const [ errorGeneral, setErrorGeneral ] = useState('');
 
   useEffect(() => {
     console.log("competencias seleccionadas en vista", competenciasSeleccionadas);
@@ -56,6 +57,7 @@ const CreacionUsuario = () => {
     control,
     handleSubmit,
     formState: { errors },
+    setError,
     trigger,
   } = useForm({
     resolver: yupResolver(esquemaCreacionUsuario),
@@ -151,15 +153,20 @@ const CreacionUsuario = () => {
       } else {
         console.log("El formulario no es válido o no se ha hecho click en 'Crear Usuario'");
       }
+      setErrorGeneral('');
     } catch (error) {
-      console.error('Error al enviar el formulario:', error);
+      // Verifica si el error es debido a un problema en el campo 'rut'
+      if (error.message && error.message.rut && error.message.rut.length > 0) {
+        // Utiliza el primer mensaje de error para el campo 'rut'
+        const mensajeErrorRut = error.message.rut[0];
+        setError('rut', { type: 'manual', message: mensajeErrorRut });
+      }
     }
   };
 
   if (isLoading) {
     return <div>Cargando...</div>;
   }
-  { error && <div className="error-message">Error al crear el usuario: {error.message}</div> }
 
   return (
     <div className="container col-10 my-4">
@@ -181,7 +188,7 @@ const CreacionUsuario = () => {
               render={({ field }) => (
                 < CustomInput
                   label="RUT (Obligatorio)"
-                  placeholder="Escribe el RUT con guión sin puntos."
+                  placeholder="Escribe el RUT con guión y sin puntos."
                   id="rut"
                   maxLength={null}
                   error={errors.rut?.message}
@@ -382,7 +389,6 @@ const CreacionUsuario = () => {
               />
             </div>
           </div>
-          {error && <div className="error-message">Error al crear el usuario: {error.message}</div>}
           <button className="btn-primario-s mb-5" type="submit" onClick={() => setSubmitClicked(true)}>
             <p className="mb-0">Crear Usuario</p>
             <i className="material-symbols-rounded ms-2">arrow_forward_ios</i>
