6eafff4e791d978b09ad5aa35564a0dbeb09a0ba
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-01-25 13:50:48 -0300

    elimina codigo comentado

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -32,16 +32,11 @@ const SidebarLink = ({ to, icon, text, badgeCount, onClick }) => {
       </NavLink>
     ) : (
       <>
-        {/* <button className="mx-4 btn-link" onClick={handleOpenModal}>
-          <i className="material-symbols-outlined mx-3">{icon}</i>
-          <p>{text}</p>
-        </button> */}
         <NavLink  className="mx-4 btn-link" onClick={handleOpenModal}>
           <i className="material-symbols-outlined mx-3">{icon}</i>
           {badgeCount && <i className="badge badge-notification mx-3">{badgeCount}</i>}
           <u>{text}</u>
         </NavLink>
-
         {isModalOpen && <ModalAbandonoFormulario onClose={handleCloseModal} isOpen={isModalOpen} direction={to}/>}
       </>
     )}
@@ -60,7 +55,6 @@ const Sidebar = () => {
   };
 
   return (
-    <>
     <div className="sidebar  fixed-top  d-flex flex-column flex-shrink-0  border-end ">
       <div className="my-0 text-start">
         <div className="line-container row ">
@@ -74,22 +68,9 @@ const Sidebar = () => {
       </div>
       <ul className="nav nav-pills flex-column mb-auto mt-0">
         <li className="my-1">
-          {/* <NavLink to="/home" className="mx-4 btn-link" aria-current="page" type="button" >
-            <i className="material-symbols-outlined mx-3 ">
-              home
-            </i> <span className="text-link">Inicio</span>
-          </NavLink> */}
           <SidebarLink to="/home" icon="home" text="Inicio" onClick={handleItemClick} />
         </li>
         <li className="my-1">
-          {/* <NavLink to="#" className="mx-4 btn-link">
-            <i className="material-symbols-outlined mx-3">
-              mail
-            </i><u>Notificaciones</u>
-
-            <i className="material-symbols-outlined mx-3"></i>
-            <i className="badge badge-notification mx-3">10</i>
-          </NavLink> */}
           <SidebarLink to="#" icon="mail" text="Notificaciones" badgeCount={10} onClick={handleItemClick} />
         </li>
         {(userSubdere || userObservador) && (
@@ -98,25 +79,9 @@ const Sidebar = () => {
             <span className="title-section  ms-4 my-1">Gestión de Usuarios</span>
           </>
         )}
-        {/* {userSubdere && (
-          <li className="my-1">
-            <NavLink to="/home/crear_usuario" className="btn-sidebar my-1 mx-4" type="button">
-              <u>Crear Usuarios </u> <i className="material-symbols-outlined">
-                person_add
-              </i>
-            </NavLink>
-          </li>)} */}
           {userSubdere && (
           <SidebarLink to="/home/crear_usuario" icon="person_add" text="Crear Usuarios" onClick={handleItemClick} />
         )}
-        {/* {(userSubdere || userObservador) && (
-          <li className="my-1">
-            <NavLink to="/home/administrar_usuarios" className="mx-4 btn-link" type="button">
-              <i className="material-symbols-outlined mx-3">supervised_user_circle</i>
-              <u>Administrar Usuarios</u>
-            </NavLink>
-          </li>
-        )} */}
         {(userSubdere || userObservador) && (
           <SidebarLink to="/home/administrar_usuarios" icon="supervised_user_circle" text="Administrar Usuarios" onClick={handleItemClick} />
         )}
@@ -125,29 +90,9 @@ const Sidebar = () => {
             <hr className="w-85 mx-4" />
             <span className="title-section ms-4 my-2">Gestión de Competencias</span>
           </>)}
-        {/* {userSubdere && (
-          <>
-            <li>
-              <NavLink to="/home/crear_competencia" className="btn-sidebar my-1 mx-4" >
-                <u><strong>Crear Competencia</strong></u><i className="material-symbols-outlined">
-                  post_add
-                </i>
-              </NavLink>
-            </li>
-          </>
-        )} */}
         {userSubdere && (
           <SidebarLink to="/home/crear_competencia" icon="post_add" text="Crear Competencia" onClick={handleItemClick} />
         )}
-        {/* {(userSubdere || userObservador) && (
-          <li className="my-1">
-            <NavLink to="/home/listado_competencias" className="mx-4 btn-link" type="button">
-              <i className="material-symbols-outlined mx-3">library_books</i>
-              <i className="badge badge-notification mx-3">99+</i>
-              <u>Listado de Competencias</u>
-            </NavLink>
-          </li>
-        )} */}
         {(userSubdere || userObservador) && (
           <SidebarLink to="/home/listado_competencias" icon="library_books" text="Listado de Competencias" badgeCount={99} onClick={handleItemClick} />
         )}
@@ -176,8 +121,6 @@ const Sidebar = () => {
         </div>
       </div>
     </div >
-    {/* {isModalOpen && <ModalAbandonoFormulario ref={modalRef} onClose={handleCloseModal} />} */}
-    </>
   );
 };
 
