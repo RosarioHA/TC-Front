67c01c35127af31d1c9d269530e46d0b18374ba2
vecarope <vecarope@mac-vcaro.local>
2024-01-30 12:11:57 -0300

    etapa 2 y redireccion formulario

--- src/context/FormSectorial.jsx
+++ src/context/FormSectorial.jsx
@@ -5,86 +5,53 @@ import { useUpdateForm } from '../hooks/formulario/useUpdateForm';
 
 export const FormularioContext = createContext();
 
-export const FormularioProvider = ({ children }) =>
-{
-  const initialId = localStorage.getItem('formId') || null;
-  const initialStepNumber = localStorage.getItem('stepNumber') || null;
-
-  const [ id, setId ] = useState(initialId);
-  const [ stepNumber, setStepNumber ] = useState(initialStepNumber);
+export const FormularioProvider = ({ children }) => {
+  const [id, setId] = useState(null);
+  const [stepNumber, setStepNumber] = useState(null);
 
   const { dataFormSectorial, loadingFormSectorial, errorFormSectorial } = useFormSectorial(id);
   const { dataPaso, loadingPaso, errorPaso } = usePasoForm(id, stepNumber);
-  const { patchStep, loading, error,createFormDataPayload } = useUpdateForm();
+  const { patchStep, loading, error } = useUpdateForm();
 
-
-  const updateFormId = (newId) =>
-  {
+  const updateFormId = useCallback((newId) => {
     setId(newId);
-    localStorage.setItem('formId', newId);
-  };
+  }, []);
 
-  const updateStepNumber = (newStepNumber) =>
-  {
+  const updateStepNumber = useCallback((newStepNumber) => {
     setStepNumber(newStepNumber);
-    localStorage.setItem('stepNumber', newStepNumber);
-  };
+  }, []);
 
 
-  const handleUpdatePaso = async (id, stepNumber, formData) =>
-  {
-    try
-    {
-      if (!formData || typeof formData !== 'object' || Object.keys(formData).length === 0)
-      {
+  const handleUpdatePaso = async (id, stepNumber, formData) => {
+    try {
+      if (!formData || typeof formData !== 'object' || Object.keys(formData).length === 0) {
         throw new Error("Datos del paso son inválidos");
       }
 
       await patchStep(id, stepNumber, formData);
-      recargarDatos();
+      // La función recargarDatos ha sido eliminada, suponiendo que patchStep ya maneja la actualización necesaria.
       return true; // Indica éxito
-    } catch (error)
-    {
+    } catch (error) {
       console.error("Error al guardar los datos:", error);
       return false; // Indica fracaso
     }
-  }
+  };
 
   const handleUploadFiles = async (id, stepNumber, archivos, fieldName) => {
-    console.log("FormData en handleUploadFiles:", archivos);
-  
     if (!archivos || !(archivos instanceof FormData) || !archivos.has(fieldName)) {
       throw new Error("No hay archivos para subir");
     }
-      // Llama a una función específica para subir archivos
-      console.log("FormData antes de la subida:", archivos);
-      const response = await patchStep(id, stepNumber, archivos);
 
-      // Verifica la respuesta para asegurar que la subida fue exitosa
-      if (response.ok)
-      {
+    // Llama a una función específica para subir archivos
+    const response = await patchStep(id, stepNumber, archivos);
 
-        recargarDatos();
-        return true; // Indica éxito
-      } else
-      {
-        // Si la subida no fue exitosa, maneja este caso adecuadamente
-        throw new Error("La subida del archivo falló");
-      }
-
-  };
-  // Función para recargar los datos actualizados
-  const recargarDatos = useCallback(async () =>
-  {
-    try
-    {
-      setId(id);
-      setStepNumber(stepNumber);
-    } catch (error)
-    {
-      console.error("Error al recargar datos:", error);
+    // Verifica la respuesta para asegurar que la subida fue exitosa
+    if (response.ok) {
+      return true; // Indica éxito
+    } else {
+      throw new Error("La subida del archivo falló");
     }
-  }, [ id, stepNumber ]);
+  };
 
   const value = {
     data: dataFormSectorial,
@@ -92,16 +59,13 @@ export const FormularioProvider = ({ children }) =>
     error: errorFormSectorial || error,
     updateFormId,
     pasoData: dataPaso,
-    recargarDatos,
     loadingPaso,
     errorPaso,
     updateStepNumber,
     stepNumber,
     handleUpdatePaso,
     handleUploadFiles,
-    createFormDataPayload
   };
-
 
   return (
     <FormularioContext.Provider value={value}>
