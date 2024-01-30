a2766c7ab5fb20c98110a736f5e5776038628748
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-01-30 19:32:52 -0300

    modifica OpcionesAB para mostrar pre seleccionada la alternativa correspondiente al editar

--- src/components/forms/opciones_AB.jsx
+++ src/components/forms/opciones_AB.jsx
@@ -1,18 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const OpcionesAB = ({ initialState, handleEstadoChange, field, altA, altB, label, readOnly }) => {
-  const [activeButton, setActiveButton] = useState(initialState);
+  const [activeButton, setActiveButton] = useState(initialState === true ? 'activo' : 'inactivo');
   console.log("initial state en componente OpcionesAB", initialState);
+
+  useEffect(() => {
+    if (initialState !== null && !readOnly) {
+      const newActiveButton = initialState === true ? 'activo' : 'inactivo';
+
+      // Verificamos si el nuevo estado es diferente al estado actual antes de actualizar
+      if (newActiveButton !== activeButton) {
+        setActiveButton(newActiveButton);
+        handleEstadoChange(initialState);
+        field.onChange(initialState === true);
+      }
+    }
+  }, [initialState, readOnly, handleEstadoChange, field, activeButton]);
 
   const handleClick = (estado) => {
     if (!readOnly) {
-      setActiveButton(prevActiveButton => {
+      setActiveButton((prevActiveButton) => {
         if (prevActiveButton === estado) {
           // Si el botón ya está activo, desactívalo
+          handleEstadoChange(null);
+          field.onChange(false);
           return null;
         } else {
           // Activa el botón
-          handleEstadoChange(estado);
+          handleEstadoChange(estado === 'activo');
           field.onChange(estado === 'activo');
           return estado;
         }
