fa27e0394bbe68cddd3a2d8f305b40444a733390
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-02-05 13:08:05 -0300

    evita error al no recibir id desde vista de origen

--- src/views/success.jsx
+++ src/views/success.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import successIcon from '../static/icons/success.svg';
@@ -6,25 +7,39 @@ const SuccessViews = () => {
   const history = useNavigate();
   const location = useLocation();
   const origen = location.state?.origen;
-  let id = location.state?.id.id;
+  let id = location.state?.id;
+
+  // Actualiza el ID si proviene de la creación de usuario
+  useEffect(() => {
+    if (origen === "crear_usuario" && id) {
+      const userId = id.id;
+      console.log("Usuario creado con ID:", userId);
+    }
+  }, [id, origen]);
 
   const handleButtonClick = () => {
-    // Realizar la navegación condicional según la procedencia del usuario
-    switch (origen) {
-      case "crear_usuario":
-        history('/home/administrar_usuarios');
-        break;
-      case "crear_competencia":
-        history('/home/listado_competencias');
-        break;
-      case "editar_usuario":
-        history(`/home/editar_usuario/${id}`);
-        break;
+    // Verifica que id tenga un valor antes de intentar acceder a sus propiedades
+    if (id && id.id) {
+      // Realizar la navegación condicional según la procedencia del usuario
+      switch (origen) {
+        case "crear_usuario":
+          history(`/home/editar_usuario/${id.id}`);
+          break;
+        case "crear_competencia":
+          history(`/home/editar_competencia/${id}`);
+          break;
+        case "editar_usuario":
+          history(`/home/editar_usuario/${id.id}`);
+          break;
         case "editar_competencia":
-        history(`/home/editar_competencia/${id}`);
-        break;
-      default:
-        history('/home');
+          history(`/home/editar_competencia/${id}`);
+          break;
+        default:
+          history('/home');
+      }
+    } else {
+      console.error("El ID del usuario es nulo o indefinido.");
+      // Puedes manejar este caso según tus necesidades, por ejemplo, redirigir a una página de error.
     }
   };
   
