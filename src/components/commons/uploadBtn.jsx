3509bd7fe76983bf92e7a0d5cc742797fa95f3a7
vecarope <vecarope@mac-vcaro.local>
2024-02-15 21:33:37 -0300

    subir archvos campo 2.5

--- src/components/commons/uploadBtn.jsx
+++ src/components/commons/uploadBtn.jsx
@@ -1,22 +1,33 @@
 import { useRef } from 'react';
 
-const UploadBtn = ({ onFileChange, fileUploaded }) => {
+const UploadBtn = ({ onFileChange, fileUploaded, onViewFile, ver }) => {
   const inputFile = useRef(null);
-  const handleButtonClick = () => { inputFile.current.click();};
+  const handleButtonClick = () => { inputFile.current.click(); };
 
   return (
     <>
       <input type="file" className="d-none" onChange={onFileChange} ref={inputFile} />
       {fileUploaded ? (
-        <button className="btn-secundario-s text-sans-p-blue d-flex px-1" type="button" onClick={handleButtonClick}>
-          <u>Modificar</u>
-          <i className="material-symbols-outlined"> edit</i>
-        </button>
+        <>
+          {ver ? (
+            // Si `ver` es true, mostrar solo el botón "Ver"
+            <button className="btn-secundario-s text-sans-p-blue d-flex px-3" type="button" onClick={onViewFile}>
+              <i className="material-symbols-outlined  me-2"> visibility </i>
+              <u>Ver</u>
+            </button>
+          ) : (
+            // Si `ver` es false o no está presente, mostrar el botón "Modificar"
+            <button className="btn-secundario-s text-sans-p-blue d-flex px-1" type="button" onClick={handleButtonClick}>
+              <u>Modificar</u>
+              <i className="material-symbols-outlined"> edit</i>
+            </button>
+          )}
+        </>
       ) : (
-        <button className="btn-secundario-s d-flex" type="button" onClick={handleButtonClick}>
-          <i className="material-symbols-outlined ">
-            upgrade
-          </i><u className="align-self-center text-sans-b-white ">Subir Archivo</u>
+        // Si no hay archivo subido, mostrar el botón para subir archivo
+        <button className="btn-secundario-s d-flex mx-2" type="button" onClick={handleButtonClick}>
+          <i className="material-symbols-outlined">upgrade</i>
+          <u className="align-self-center text-sans-b-white">Subir Archivo</u>
         </button>
       )}
     </>
