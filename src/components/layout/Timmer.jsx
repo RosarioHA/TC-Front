2289aa6367f6a970f39c1c1f11b29c2dc1eb1ddb
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-02-27 10:50:56 -0300

    vista Resumen Obs Subdere

--- src/components/layout/Timmer.jsx
+++ src/components/layout/Timmer.jsx
@@ -10,7 +10,7 @@ export const Timmer = ({data, error }) =>
 
   return (
     <>
-      <div className=" mt-4 border-bottom">
+      <div className=" mt-4">
         <div className="w-75 mx-auto mb-5">
           <Counter plazoDias={plazoDias} tiempoTranscurrido={tiempoTranscurrido} />
           <div className="text-sans-p">Fecha última modificación: {data.fecha_ultima_modificacion}</div>
--- src/views/formularioSectorial/ResumenOS.jsx
+++ src/views/formularioSectorial/ResumenOS.jsx
@@ -1,10 +1,14 @@
 import { useContext, useEffect } from "react";
 import { FormularioContext } from '../../context/FormSectorial';
 import { useObservacionesSubdere } from "../../hooks/formulario/useObSubdereSectorial";
+import CustomTextarea from "../../components/forms/custom_textarea";
+import successIcon from '../../static/icons/success.svg';
+import { useNavigate } from "react-router-dom";
 
 const ResumenOS = () => {
 	const { data } = useContext(FormularioContext);
 	const { observaciones, fetchObservaciones } = useObservacionesSubdere(data ? data.id : null);
+	const navigate = useNavigate();
 
   useEffect(() => {
     const obtenerObservaciones = async () => {
@@ -14,17 +18,76 @@ const ResumenOS = () => {
         console.error("Error al obtener observaciones en ResumenOS:", error);
       }
     };
-
     obtenerObservaciones();
   }, [data, fetchObservaciones]);
 
-  console.log("ID en ResumenOS:", data ? data.id : "No hay ID");
-  console.log("observaciones subdere en resumen os", observaciones);
+	const handleBackButtonClick = () => {
+    navigate(-1);
+  };
 
   return (
-    <div className="container">
-      <p className="text-sans-h1">Resumen observaciones </p>
-			<hr/>
+    <div className="container col-11">
+      <p className="text-sans-h1 text-center">Resumen observaciones </p>
+
+      {Object.keys(observaciones).map((pasoKey) => {
+        // Excluir claves no deseadas
+        if (
+					pasoKey === 'observacion_enviada' || 
+					pasoKey === 'fecha_envio' || 
+					pasoKey === 'id') {
+          return null;
+        }
+
+        const pasoNumber = pasoKey.replace('observacion_paso', ''); 
+        const observacion = observaciones[pasoKey];
+
+				if (Array.isArray(observacion) && observacion.length === 0) {
+          return null; // No mostrar nada si es un array vacío
+        }
+
+        return (
+          <div key={pasoKey}>
+						<hr/>
+						<div className="d-flex justify-content-between align-items-center mb-3">
+							<p className="">Paso {pasoNumber}</p>
+							{console.log("Valor de observacion:", observacion)}
+							{observacion && !Array.isArray(observacion) && (
+							<>
+								<img className="icono-s" src={successIcon} alt="Éxito" />
+								<p className="text-sans-p-blue me-3">Listo</p>
+							</>
+							)}
+							{observacion != null && (Array.isArray(observacion) ? observacion.length === 0 : observacion.trim() === "") && (
+								<p className="text-sans-p me-3">No dejaste observaciones en este paso.</p>
+							)}
+						</div>
+            {observacion && !Array.isArray(observacion) && (
+              <CustomTextarea
+                label="Observaciones (Opcional)"
+                type="text"
+                id={`paso${pasoNumber}`}
+                name={`paso${pasoNumber}`}
+                value={observacion}
+                readOnly
+                className="form-control"
+              />
+            )}
+          </div>
+        );
+      })}
+
+			<h2 className="text-sans-h2 mt-5">Esta todo listo para que envies las observaciones</h2>
+			<p className="text-sans-p">Asegurate que las observaciones que ingresaste son suficientes, ya que una vez que las envíes, no podrás editarlas.</p>
+			<div className="d-flex justify-content-between p-2 mb-5 pb-5">
+				<button className="d-flex btn-secundario-s"  onClick={handleBackButtonClick}>
+					<i className="material-symbols-rounded me-2">arrow_back_ios</i>
+					Atrás
+				</button>
+				<button className="d-flex btn-primario-s">
+					Enviar Observaciones
+					<i className="material-symbols-rounded me-2">arrow_forward_ios</i>
+				</button>
+			</div>
     </div>
   );
 }
