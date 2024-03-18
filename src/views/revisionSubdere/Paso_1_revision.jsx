3104e564b0a175e03768a1da966bcc175529a569
vecarope <vecarope@mac-vcaro.local>
2024-03-18 15:17:05 -0300

    layout base revision subdere'

---
+++ src/views/revisionSubdere/Paso_1_revision.jsx
@@ -0,0 +1,8 @@
+
+const Paso_1_revision = () => {
+  return (
+    <div>paso_1_revision</div>
+  )
+}
+
+export default Paso_1_revision; 
---
+++ src/views/revisionSubdere/Paso_2_revision.jsx
@@ -0,0 +1,9 @@
+
+const Paso_2_Revision = () =>
+{
+  return (
+    <div>paso_2_revision</div>
+  )
+}
+
+export default Paso_2_Revision
