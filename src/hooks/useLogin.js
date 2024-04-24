788483b9bc79e493d62d70166bc9582e8c901999
vecarope <vecarope@mac-vcaro.local>
2024-04-24 16:20:42 -0400

    prueba 2 bugs QA

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,45 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 import { apiTransferenciaCompentencia } from "../services/transferenciaCompetencia";
 
-export const useLogin = () =>
-{
+export const useLogin = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const [ data, setData ] = useState(null);
-  const [ loading, setLoading ] = useState(false);
-  const [ error, setError ] = useState(null);
+  const login = async ({ rut, password }) => {
+    if (!rut || !password) {
+      setError("RUT y contraseña son necesarios.");
+      return; // Evitar la ejecución si los campos necesarios están vacíos
+    }
 
-
-  const login = async ({ rut, password }) =>
-  {
     setLoading(true);
-    try
-    {
+    setError(null); // Limpiar errores anteriores antes de una nueva solicitud
+    try {
       const response = await apiTransferenciaCompentencia.post('login/', {
-        rut: rut,
-        password: password
+        rut,
+        password
       });
       setData(response.data);
-      localStorage.setItem('useToken', response.data.token);
-      localStorage.setItem('refreshToken', response.data[ 'refresh-token' ]);
-    } catch (error)
-    {
-      if (error.response)
-      {
-        console.error('Error data:', error.response.data);
-        console.error('Error status:', error.response.status);
-        console.error('Error headers:', error.response.headers);
-        setError(error.response);
-      } else
-      {
-        console.error('Error:', error.message);
-        setError(error);
-      }
-    } finally
-    {
+      localStorage.setItem('userToken', response.data.token);
+      localStorage.setItem('refreshToken', response.data['refresh-token']);
+    } catch (error) {
+      const errMsg = error.response ? error.response.data.error || "Error al conectar con el servidor" : "Error en la conexión con el servidor. Intenta de nuevo más tarde.";
+      console.error('Error en login:', errMsg);
+      setError(errMsg); 
+    } finally {
       setLoading(false);
     }
   };
-
 
   return { data, loading, error, login };
 }
