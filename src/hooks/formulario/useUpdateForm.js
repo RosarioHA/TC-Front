c58cd7310ef13d38f515d69b1cfde31521a07e7f
vecarope <vecarope@mac-vcaro.local>
2024-04-25 15:41:09 -0400

    prueba localstorage

--- src/hooks/formulario/useUpdateForm.js
+++ src/hooks/formulario/useUpdateForm.js
@@ -1,62 +1,46 @@
 import { useState } from "react";
 import { apiTransferenciaCompentencia } from "../../services/transferenciaCompetencia";
 
-export const useUpdateForm = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [success, setSuccess] = useState(false);
+export const useUpdateForm = () =>
+{
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState(null);
+  const [ success, setSuccess ] = useState(false);
+  const [ response, setResponse ] = useState(null);
 
-  const createFormDataPayload = (datosPaso, archivos) => {
-    const payload = new FormData();
-
-    // Agregar archivos al FormData
-    for (let key of archivos.keys()) {
-      const file = archivos.get(key);
-      if (Array.isArray(file)) {
-        // Para campos múltiples (si es necesario)
-        file.forEach((fileItem) => {
-          if (fileItem instanceof File) {
-            payload.append(key, fileItem);
-          }
-        });
-      } else if (file instanceof File) {
-        payload.append(key, file);
-      }
-    }
-
-    // Agregar datos al FormData
-    for (const key in datosPaso) {
-      if (Object.prototype.hasOwnProperty.call(datosPaso, key)) {
-        const value = datosPaso[key];
-        if (typeof value === 'object' && !(value instanceof File)) {
-          payload.append(key, JSON.stringify(value)); // Para objetos JSON
-        } else {
-          payload.append(key, value);
-        }
-      }
-    }
-
-    return payload;
-  };
-
-  const patchStep = async (id, stepNumber, payload) => {
+  const patchStep = async (id, stepNumber, data, formData = null) =>
+  {
     setLoading(true);
     setError(null);
-    setSuccess(false); // Reiniciar el estado de éxito
-  
-    try {
-      const response = await apiTransferenciaCompentencia.patch(`/formulario-sectorial/${id}/paso-${stepNumber}/`, payload);
-  
-      setLoading(false);
-      setSuccess(true); // Actualizar el estado de éxito
-      return response.data;
-    } catch (err) {
-      setLoading(false);
-      const errMsg = err.response ? err.response.data : err.message;
-      setError(errMsg);
-      throw new Error(errMsg);
-    }
+    setSuccess(false);
 
-  }
-  return { patchStep, loading, error, createFormDataPayload , success};
+    try
+    {
+      const url = `/formulario-sectorial/${id}/paso-${stepNumber}/`;
+      let headers = {};
+
+      let payload = data;
+      if (formData)
+      {
+        payload = formData;
+        headers[ 'Content-Type' ] = 'multipart/form-data';
+      } else
+      {
+        headers[ 'Content-Type' ] = 'application/json';
+        payload = JSON.stringify(data);
+      }
+
+      const response = await apiTransferenciaCompentencia.patch(url, payload, { headers });
+      setResponse(response.data);
+      setSuccess(true);
+    } catch (error)
+    {
+      setError(error.response ? error.response.data : error.message);
+    } finally
+    {
+      setLoading(false);
+    }
+  };
+  return { patchStep, loading, error, success, response };
 };
+
