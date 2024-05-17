6ff34afb471e5ad576f2c49fd24ce61ba647faaa
vecarope <vecarope@mac-vcaro.local>
2024-05-17 17:36:13 -0400

    separador por regiones paso 3 y 4

--- src/views/formularioSectorial/pasoCuatro.jsx
+++ src/views/formularioSectorial/pasoCuatro.jsx
@@ -8,47 +8,62 @@ import CustomTextarea from '../../components/forms/custom_textarea';
 import { useAuth } from '../../context/AuthContext';
 import { useObservacionesSubdere } from '../../hooks/formulario/useObSubdereSectorial';
 
-const PasoCuatro = () => {
+const PasoCuatro = () =>
+{
   const { updateStepNumber, pasoData, data } = useContext(FormularioContext);
   const stepNumber = 4;
   const { userData } = useAuth();
   const userSubdere = userData?.perfil?.includes('SUBDERE');
   const userSectorial = userData?.perfil?.includes('Sectorial');
   const { observaciones, updateObservacion, fetchObservaciones, loadingObservaciones, saved } = useObservacionesSubdere(data ? data.id : null);
-  const [observacionPaso4, setObservacionPaso4] = useState("");
+  const [ observacionPaso4, setObservacionPaso4 ] = useState("");
+  const [ collapseStates, setCollapseStates ] = useState({});
+
+  const toggleCollapse = (index) =>
+  {
+    setCollapseStates(prevState => ({
+      ...prevState,
+      [ index ]: !prevState[ index ]
+    }));
+  };
 
   const observacionesEnviadas = observaciones.observacion_enviada
   const formSectorialEnviado = data?.formulario_enviado
 
-  useEffect(() => {
+  useEffect(() =>
+  {
     updateStepNumber(stepNumber);
-    if (observaciones && Object.keys(observaciones).length === 0) {
+    if (observaciones && Object.keys(observaciones).length === 0)
+    {
       fetchObservaciones();
     }
-    if (observaciones && observaciones.observacion_paso4) {
+    if (observaciones && observaciones.observacion_paso4)
+    {
       setObservacionPaso4(observaciones.observacion_paso4);
     }
-  }, [updateStepNumber, stepNumber, observaciones, fetchObservaciones]);
+  }, [ updateStepNumber, stepNumber, observaciones, fetchObservaciones ]);
 
   if (!pasoData) return <div>No hay datos disponibles para el Paso 4</div>;
 
   const { paso4: paso4Data, indicador_desempeno, lista_indicadores, solo_lectura } = pasoData;
   if (!paso4Data) return <> <div className="d-flex align-items-center flex-column my-5 px-5 ">
-  <div className="text-center text-sans-h5-medium-blue ">Cargando paso 4</div>
-  <span className="placeholder col-6 bg-primary"></span>
-</div></>;
+    <div className="text-center text-sans-h5-medium-blue ">Cargando paso 4</div>
+    <span className="placeholder col-6 bg-primary"></span>
+  </div></>;
 
   const id = data?.id;
 
-  const handleGuardarObservacion = async () => {
+  const handleGuardarObservacion = async () =>
+  {
     const observacionData = {
       observacion_paso4: observacionPaso4,
     };
     await updateObservacion(observacionData);
   };
 
-  const avance = pasoData?.paso4?.avance; 
+  const avance = pasoData?.paso4?.avance;
 
+  const index= 1; 
   return (
     <>
       <div className="col-1">
@@ -58,46 +73,67 @@ const PasoCuatro = () => {
         <div className="container vh-100">
           <div className="d-flex">
             <h3 className="mt-3">{paso4Data.nombre_paso}</h3>
-            <Avance avance={avance}  id={id}/>
+            <Avance avance={avance} id={id} />
           </div>
           <div className="mt-4">
             <h6 className="text-sans-h6-primary">Los indicadores de desempeño, deben incluir una descripción de los componentes del indicador, asi como los medios utilizados para su calculo y sus medios de verificación. Si la competencia esta asociada a un programa que cuente con evaluación ex ante, se debe considerar la información incluida en su versión mas actualizada.</h6>
             <h6 className="text-sans-h6-primary mt-3">De no contar la competencia con indicadores de desempeño asociados, este apartado debe ser omitido.</h6>
             <h6 className="text-sans-h6-primary mt-3">Si el ejercicio de la competencia tiene mas de un indicador de desempeño, se deben añadir las tablas correspondientes.</h6>
             <div className="my-5 pb-3 border-bottom">
-              <div className="">
-                {/* Componente adicional en blanco para un nuevo indicador */}
-                {indicador_desempeno && (
-                  <Subpaso_CuatroUno
-                    data={pasoData}
-                    id={id}
-                    stepNumber={stepNumber}
-                    listaIndicadores={lista_indicadores}
-                    readOnly={false}
-                    solo_lectura = {solo_lectura}
-                  />
-                )}
+
+              <div className="col-12 collapse-regiones border  border-bottom" key={index} type="button" data-bs-toggle="collapse"
+                data-bs-target={`#collapseRegion${index}`} aria-expanded={collapseStates[ index ] ? "true" : "false"}
+                aria-controls={`collapseRegion${index}`} onClick={() => toggleCollapse(index)}>
+                <div className="d-flex justify-content-between">
+                  <span className="text-sans-h4 text-start">Nombre region </span>
+                  <button className="btn-secundario-s-round">
+                    {collapseStates[ index ] ? "Ocultar sección" : "Mostrar sección"}
+                    <span className="material-symbols-outlined">
+                      {collapseStates[ index ] ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+                    </span>
+                  </button>
+                </div>
+                <div>
+                  <Avance avance={avance} id={id} />
+                </div>
+              </div>
+              <div className={`collapse ${collapseStates[ index ] ? 'show' : ''}`} id={`collapseRegion${index}`}>
+                <div className="card card-body ">
+                  <>
+                    {/* Componente adicional en blanco para un nuevo indicador */}
+                    {indicador_desempeno && (
+                      <Subpaso_CuatroUno
+                        data={pasoData}
+                        id={id}
+                        stepNumber={stepNumber}
+                        listaIndicadores={lista_indicadores}
+                        readOnly={false}
+                        solo_lectura={solo_lectura}
+                      />
+                    )}
+                  </>
+                </div>
               </div>
             </div>
           </div>
 
-          { (userSubdere || (userSectorial && formSectorialEnviado)) && (
+          {(userSubdere || (userSectorial && formSectorialEnviado)) && (
             <div className="mt-5 my-4">
               {!observacionPaso4.trim() && observacionesEnviadas ? (
                 <p>No se han dejado observaciones en este paso.</p>
               ) : (
-              <CustomTextarea 
-              label="Observaciones (Opcional)"
-              placeholder="Escribe tus observaciones de este paso del formulario"
-              rows={5}
-              maxLength={500}
-              value={observacionPaso4}
-              onChange={(e) => setObservacionPaso4(e.target.value)}
-              readOnly={observacionesEnviadas}
-              onBlur={handleGuardarObservacion}
-              loading={loadingObservaciones}
-              saved={saved}
-              />
+                <CustomTextarea
+                  label="Observaciones (Opcional)"
+                  placeholder="Escribe tus observaciones de este paso del formulario"
+                  rows={5}
+                  maxLength={500}
+                  value={observacionPaso4}
+                  onChange={(e) => setObservacionPaso4(e.target.value)}
+                  readOnly={observacionesEnviadas}
+                  onBlur={handleGuardarObservacion}
+                  loading={loadingObservaciones}
+                  saved={saved}
+                />
               )}
             </div>
           )}
