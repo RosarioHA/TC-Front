465ee926ceff9610b2a182488437c8a169c95fac
vecarope <vecarope@mac-vcaro.local>
2024-06-04 16:29:36 -0400

    arreglar tamaño de input dentro del col lapse

--- src/views/formularioSectorial/pasoTres.jsx
+++ src/views/formularioSectorial/pasoTres.jsx
@@ -59,7 +59,7 @@ const PasoTres = () => {
   const transformData = (regiones) => {
     return regiones.reduce((acc, region) => {
       acc[region.region] = {
-        paso3: region.paso3.map((p3) => ({
+        paso3: region.paso3?.map((p3) => ({
           id: p3.id,
           universo_cobertura: p3.universo_cobertura || '',
           descripcion_cobertura: p3.descripcion_cobertura || '',
@@ -75,7 +75,7 @@ const PasoTres = () => {
       ...prevFormData,
       [region]: {
         ...prevFormData[region],
-        paso3: prevFormData[region].paso3.map((p3) => ({
+        paso3: prevFormData[region]?.paso3.map((p3) => ({
           ...p3,
           [inputName]: value,
         })),
@@ -99,7 +99,7 @@ const PasoTres = () => {
       regiones: [
         {
           region: region,
-          paso3: formData[region].paso3,
+          paso3: formData[region]?.paso3,
         },
       ],
     };
@@ -159,172 +159,167 @@ const PasoTres = () => {
         <MonoStepers stepNumber={paso3encabezado?.numero_paso} />
       </div>
       <div className="col-11">
-          <div className="d-flex">
-            <h3 className="mt-3 me-4">{paso3encabezado?.nombre_paso}</h3>
-            <Avance avance={avance} id={id} />
-          </div>
-          <div className="container text-sans-h6-primary">
-            <h6>
-              Este apartado tiene por objetivo conocer y cuantificar la
-              cobertura de la competencia considerando sus diferentes unidades
-              de medición. Se debe comparar el universo de cobertura con la
-              cobertura efectivamente abordada en el ejercicio de la
-              competencia.
-              <br />
-              <br />
-              Aquellas competencias que estén orientadas a una población
-              objetivo, se debe identificar la población potencial, así como los
-              mecanismos para cuantificarla y seleccionar a los beneficiarios/as
-              finales. Las competencias que tengan otras unidades de medida
-              deben realizar la misma tarea.
-              <br />
-              <br />
-              Si la competencia está asociada a un programa que cuente con
-              evaluación ex ante, se debe considerar la información más
-              actualizada.
-            </h6>
-          </div>
-          {regiones?.map((region, index) => (
-            <div key={index} className="my-5">
-              <div
-                className="col-12 collapse-regiones border  border-bottom"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target={`#collapseRegion${index}`}
-                aria-expanded={collapseStates[index] ? 'true' : 'false'}
-                aria-controls={`collapseRegion${index}`}
-                onClick={() => toggleCollapse(index)}
-              >
-                <div className="d-flex justify-content-between">
-                  <span className="text-sans-h4 text-start">
-                    {region?.region}
-                  </span>
-                  <button className="btn-secundario-s-round">
-                    {collapseStates[index]
-                      ? 'Ocultar sección'
-                      : 'Mostrar sección'}
-                    <span className="material-symbols-outlined">
-                      {collapseStates[index]
-                        ? 'keyboard_arrow_up'
-                        : 'keyboard_arrow_down'}
-                    </span>
-                  </button>
-                </div>
-                <div>
-                  <Avance avance={region?.paso3[0]?.avance} id={id} />
-                </div>
-              </div>
-              <div
-                className={`collapse ${collapseStates[index] ? 'show' : ''}`}
-                id={`collapseRegion${index}`}
-              >
-                <div className="card card-body">
-                  <div>
-                    <div className="my-2 me-4">
-                      <CustomTextarea
-                        label="Descripción de universo de cobertura (Obligatorio)"
-                        placeholder="Describe el universo de cobertura"
-                        name="universo_cobertura"
-                        id="universo_cobertura"
-                        value={
-                          formData[region.region]?.paso3[0]
-                            ?.universo_cobertura || ''
-                        }
-                        onChange={(e) =>
-                          handleChange(region.region, `universo_cobertura`, e)
-                        }
-                        onBlur={() =>
-                          handleSave(region.region, `universo_cobertura`)
-                        }
-                        loading={inputStatus.universo_cobertura.loading}
-                        saved={inputStatus.universo_cobertura.saved}
-                        maxLength={800}
-                        readOnly={solo_lectura}
-                        rows={2}
-                      />
-                      <div className="d-flex mb-3 mt-0 text-sans-h6-primary">
-                        <i className="material-symbols-rounded me-2">info</i>
-                        <h6 className="mt-0">
-                          La descripción del universo de cobertura debe
-                          responder preguntas tales como: ¿Cuál es el universo?
-                          ¿Cómo se identifica?.
-                        </h6>
-                      </div>
-                    </div>
-                    <div className="my-4 me-4">
-                      <CustomTextarea
-                        label="Descripción de cobertura efectivamente abordada (Obligatorio)"
-                        placeholder="Describe la cobertura efectivamente abordada"
-                        id="descripcion_cobertura"
-                        name="descripcion_cobertura"
-                        value={
-                          formData[region.region]?.paso3[0]
-                            ?.descripcion_cobertura || ''
-                        }
-                        onChange={(e) =>
-                          handleChange(
-                            region.region,
-                            `descripcion_cobertura`,
-                            e
-                          )
-                        }
-                        onBlur={() =>
-                          handleSave(region.region, `descripcion_cobertura`)
-                        }
-                        loading={inputStatus.descripcion_cobertura.loading}
-                        saved={inputStatus.descripcion_cobertura.saved}
-                        maxLength={800}
-                        readOnly={solo_lectura}
-                        rows={2}
-                      />
-                      <div className="d-flex mb-3 mt-0 text-sans-h6-primary">
-                        <i className="material-symbols-rounded me-2">info</i>
-                        <h6 className="mt-0">
-                          La descripción de la cobertura efectiva debe responder
-                          preguntas tales como: ¿Cuál es? ¿Cómo se selecciona?.
-                        </h6>
-                      </div>
-                    </div>
-                    <>
-                      <Subpaso_Tres
-                        esquemaDatos={region?.cobertura_anual}
-                        region={region?.region}
-                        id={id}
-                        stepNumber={stepNumber}
-                        solo_lectura={solo_lectura}
-                      />
-                    </>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-          {(userSubdere || (userSectorial && formSectorialEnviado)) && (
-            <div className="mt-5 my-4">
-              {!observacionPaso3.trim() && observacionesEnviadas ? (
-                <p>No se han dejado observaciones en este paso.</p>
-              ) : (
-                <CustomTextarea
-                  label="Observaciones (Opcional)"
-                  placeholder="Escribe tus observaciones de este paso del formulario"
-                  rows={5}
-                  maxLength={500}
-                  value={observacionPaso3}
-                  onChange={(e) => setObservacionPaso3(e.target.value)}
-                  readOnly={observacionesEnviadas}
-                  onBlur={handleGuardarObservacion}
-                  loading={loadingObservaciones}
-                  saved={saved}
-                />
-              )}
-            </div>
-          )}
-
-          {/* Botones navegación */}
-          <div className="container me-5 pe-5">
-            <ButtonsNavigate step={paso3encabezado?.numero_paso} id={id} />
-          </div>
+        <div className="d-flex">
+          <h3 className="mt-3 me-4">{paso3encabezado?.nombre_paso}</h3>
+          <Avance avance={avance} id={id} />
         </div>
+        <div className="container text-sans-h6-primary">
+          <h6>
+            Este apartado tiene por objetivo conocer y cuantificar la cobertura
+            de la competencia considerando sus diferentes unidades de medición.
+            Se debe comparar el universo de cobertura con la cobertura
+            efectivamente abordada en el ejercicio de la competencia.
+            <br />
+            <br />
+            Aquellas competencias que estén orientadas a una población objetivo,
+            se debe identificar la población potencial, así como los mecanismos
+            para cuantificarla y seleccionar a los beneficiarios/as finales. Las
+            competencias que tengan otras unidades de medida deben realizar la
+            misma tarea.
+            <br />
+            <br />
+            Si la competencia está asociada a un programa que cuente con
+            evaluación ex ante, se debe considerar la información más
+            actualizada.
+          </h6>
+        </div>
+        {regiones?.map((region, index) => (
+          <div key={index} className="my-5">
+            <div
+              className="col-12 collapse-regiones border  border-bottom"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target={`#collapseRegion${index}`}
+              aria-expanded={collapseStates[index] ? 'true' : 'false'}
+              aria-controls={`collapseRegion${index}`}
+              onClick={() => toggleCollapse(index)}
+            >
+              <div className="d-flex justify-content-between">
+                <span className="text-sans-h4 text-start">
+                  {region?.region}
+                </span>
+                <button className="btn-secundario-s-round">
+                  {collapseStates[index]
+                    ? 'Ocultar sección'
+                    : 'Mostrar sección'}
+                  <span className="material-symbols-outlined">
+                    {collapseStates[index]
+                      ? 'keyboard_arrow_up'
+                      : 'keyboard_arrow_down'}
+                  </span>
+                </button>
+              </div>
+              <div>
+                <Avance avance={region?.paso3?.[0]?.avance} id={id} />
+              </div>
+            </div>
+            <div
+              className={`collapse ${collapseStates[index] ? 'show' : ''}`}
+              id={`collapseRegion${index}`}
+            >
+              <div className="card card-body">
+                <div className="row">
+                  <div className="col-12 mt-3">
+                    <CustomTextarea
+                      label="Descripción de universo de cobertura (Obligatorio)"
+                      placeholder="Describe el universo de cobertura"
+                      name="universo_cobertura"
+                      id="universo_cobertura"
+                      value={
+                        formData[region.region]?.paso3?.[0]
+                          ?.universo_cobertura || ''
+                      }
+                      onChange={(e) =>
+                        handleChange(region.region, `universo_cobertura`, e)
+                      }
+                      onBlur={() =>
+                        handleSave(region.region, `universo_cobertura`)
+                      }
+                      loading={inputStatus.universo_cobertura.loading}
+                      saved={inputStatus.universo_cobertura.saved}
+                      maxLength={800}
+                      readOnly={solo_lectura}
+                      rows={2}
+                    />
+                    <div className="d-flex mb-3 mt-0 text-sans-h6-primary">
+                      <i className="material-symbols-rounded me-2">info</i>
+                      <h6 className="mt-0">
+                        La descripción del universo de cobertura debe responder
+                        preguntas tales como: ¿Cuál es el universo? ¿Cómo se
+                        identifica?.
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="mt-3 me-4">
+                    <CustomTextarea
+                      label="Descripción de cobertura efectivamente abordada (Obligatorio)"
+                      placeholder="Describe la cobertura efectivamente abordada"
+                      id="descripcion_cobertura"
+                      name="descripcion_cobertura"
+                      value={
+                        formData[region.region]?.paso3?.[0]
+                          ?.descripcion_cobertura || ''
+                      }
+                      onChange={(e) =>
+                        handleChange(region.region, `descripcion_cobertura`, e)
+                      }
+                      onBlur={() =>
+                        handleSave(region.region, `descripcion_cobertura`)
+                      }
+                      loading={inputStatus.descripcion_cobertura.loading}
+                      saved={inputStatus.descripcion_cobertura.saved}
+                      maxLength={800}
+                      readOnly={solo_lectura}
+                      rows={2}
+                    />
+                    <div className="d-flex mb-3 mt-0 text-sans-h6-primary">
+                      <i className="material-symbols-rounded me-2">info</i>
+                      <h6 className="mt-0">
+                        La descripción de la cobertura efectiva debe responder
+                        preguntas tales como: ¿Cuál es? ¿Cómo se selecciona?.
+                      </h6>
+                    </div>
+                  </div>
+                  <>
+                    <Subpaso_Tres
+                      esquemaDatos={region?.cobertura_anual}
+                      region={region?.region}
+                      id={id}
+                      stepNumber={stepNumber}
+                      solo_lectura={solo_lectura}
+                    />
+                  </>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+        {(userSubdere || (userSectorial && formSectorialEnviado)) && (
+          <div className="mt-5 my-4">
+            {!observacionPaso3.trim() && observacionesEnviadas ? (
+              <p>No se han dejado observaciones en este paso.</p>
+            ) : (
+              <CustomTextarea
+                label="Observaciones (Opcional)"
+                placeholder="Escribe tus observaciones de este paso del formulario"
+                rows={5}
+                maxLength={500}
+                value={observacionPaso3}
+                onChange={(e) => setObservacionPaso3(e.target.value)}
+                readOnly={observacionesEnviadas}
+                onBlur={handleGuardarObservacion}
+                loading={loadingObservaciones}
+                saved={saved}
+              />
+            )}
+          </div>
+        )}
+
+        {/* Botones navegación */}
+        <div className="container me-5 pe-5">
+          <ButtonsNavigate step={paso3encabezado?.numero_paso} id={id} />
+        </div>
+      </div>
     </>
   );
 };
