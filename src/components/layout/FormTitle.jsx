70d5cfbd0a415b624106fe3f833f61fce7cd6775
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-06-10 14:11:33 -0400

    muestra competencias del grupo en desplegable para competencias agrupadas, componente FormTitle

--- src/components/layout/FormTitle.jsx
+++ src/components/layout/FormTitle.jsx
@@ -1,16 +1,74 @@
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCompetencia } from '../../hooks/competencias/useCompetencias';
 
 export const FormTitle = ({ data, error, title }) => {
   const navigate = useNavigate();
   const handleBackButtonClick = () => { 
     navigate(-1); 
   }
-
+  const id = data?.competencia_id;
+  const {competenciaDetails }  = useCompetencia(id);
+  const [competenciasCollapsed, setCompetenciasCollapsed] = useState(false);
+  const competenciasAgrupadas = competenciaDetails?.competencias_agrupadas;
+  const numCompetenciasAgrupadas = competenciasAgrupadas ? competenciasAgrupadas.length : 0;
+  
   if (error) return <div>Error al cargar los datos: {error.message}</div>;
   if (!data) return <div>No hay datos disponibles</div>; 
 
-  console.log("data en FormTitle", data)
-  console.log("title en FormTitle", title)
+  console.log("competenciaDetails en FormTitle", competenciaDetails) // data de la competencia que necesitamos
+  console.log("competenciasAgrupadas en FormTitle", competenciasAgrupadas);
+  console.log("numCompetenciasAgrupadas en FormTitle", numCompetenciasAgrupadas);
+  
+  const toggleCompetenciasCollapse = () => {
+    setCompetenciasCollapsed(!competenciasCollapsed);
+  };
+
+  const renderCompetenciasAgrupadas = () => {
+    //competencias individuales CHECK
+    if (competenciasAgrupadas && competenciasAgrupadas.length === 0) {
+      return (
+        <div className="">
+          <span className="text-sans-h1 mb-1">{competenciaDetails?.nombre}</span>
+        </div>
+      );
+    //competencias agrupadas
+    } else if (competenciasAgrupadas?.length > 0) {
+    return(
+      <div>
+        <button 
+        type="button" 
+        onClick={toggleCompetenciasCollapse}
+        className="btn d-flex justify-content-between w-100 px-0"
+        >
+          <span className="text-sans-h1 mb-1">{competenciaDetails?.nombre}</span>
+          <div className="d-flex align-items-center">
+            <span className="badge-info-expandibles">
+              <p className="my-0">{numCompetenciasAgrupadas} competencias</p>
+            </span>
+            <span className="material-symbols-outlined text-black ms-2">
+              {competenciasCollapsed ? 'expand_less' : 'expand_more'}
+            </span>
+          </div>
+        </button>
+        <div className={`collapse ${competenciasCollapsed ? 'show' : ''}`}>
+          <table className="table table-striped">
+            <tbody>
+              {competenciasAgrupadas.map((competencia, index) => (
+                <tr key={index}>
+                  <td className="d-flex justify-content-between">
+                    <span className="text-sans-h5 mb-1">{competencia.nombre}</span>
+                    <button>boton? a donde?</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )}
+    return null;
+  };
 
   return (
     <div className="my-3 mx-3">
@@ -33,8 +91,11 @@ export const FormTitle = ({ data, error, title }) => {
       </div>
       <span className="text-sans-Title mt-3">{title}</span>
       <div className="my-3 mb-5">
-        <div className="text-sans-h1 mb-1">{data.competencia_nombre}</div>
+        <div className="">
+          {renderCompetenciasAgrupadas()}
+        </div>
         <div className="text-sans-h2-grey mb-2">{data.sector_nombre || data.region_nombre}</div>
+        <div>AQUI TIENE QUE IR LAS REGIONES/SECTORES SEGUN SI ES FORM SECTORIAL O GORE</div>
       </div>
     </div>
   );
