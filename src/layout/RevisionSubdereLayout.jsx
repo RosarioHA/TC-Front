699d8f8073f8cc27fd30514f738a3b2305c96db0
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-06-11 16:27:05 -0400

    agrega breadcrumbs y boton volver a RevisionSubdereLayout

--- src/layout/RevisionSubdereLayout.jsx
+++ src/layout/RevisionSubdereLayout.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet, useLocation, useParams, useNavigate, Link } from 'react-router-dom';
 import { Timmer } from "../components/layout/Timmer"
 import { HorizontalRevision } from "../components/stepers/HorizontalRevision";
 import { FormSubdereContext } from '../context/RevisionFinalSubdere';
@@ -9,6 +9,9 @@ const FormLayout = () => {
   const { dataFormSubdere, dataPasoSubdere, loadingFormSubdere, errorFormSubdere, updateFormId, permisoPaso2,setPermisoPaso2 } = useContext(FormSubdereContext);
   const location = useLocation();
   const params = useParams();
+  const navigate = useNavigate();
+
+  console.log("dataFormSubdere en rev final layout", dataFormSubdere);
 
   useEffect(() => {
     const currentId = location.state?.id || params.id;
@@ -32,8 +35,28 @@ const FormLayout = () => {
   </div>;  
   }
 
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="container col-10 col-xxl-11">
+      <div className="py-3 d-flex">
+        <div className="align-self-center">
+          <button className="btn-secundario-s" onClick={handleBackButtonClick}>
+            <i className="material-symbols-rounded me-2">arrow_back_ios</i>
+            <p className="mb-0 text-decoration-underline">Volver</p>
+          </button>
+        </div>
+        <nav className="container mx-5" aria-label="breadcrumb">
+          <ol className="breadcrumb breadcrumb-style d-flex my-2">
+            <li className="breadcrumb-item align-self-center"><Link to="/home">Inicio</Link></li>
+            <li className="breadcrumb-item align-self-center"><Link to={`/home/estado_competencia/${params.id}`}>Estado de la Competencia: {dataFormSubdere.competencia_nombre} </Link></li>
+            <li className="breadcrumb-item align-self-center text-sans-p-lightgrey" aria-current="page">Revisión final SUBDERE</li>
+          </ol>
+        </nav>
+      </div>
+
       <div className="row">
         <div className="col mb-2">
           <div className="border-bottom my-3">
