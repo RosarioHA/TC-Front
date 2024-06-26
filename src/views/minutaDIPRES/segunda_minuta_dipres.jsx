f68dba5d5b7fc4505144463de6dbf2995ccd3d5c
vecarope <vecarope@mac-vcaro.local>
2024-06-26 17:20:40 -0400

    badges finalizar para observador

--- src/views/minutaDIPRES/segunda_minuta_dipres.jsx
+++ src/views/minutaDIPRES/segunda_minuta_dipres.jsx
@@ -6,33 +6,40 @@ import { useEtapa5 } from "../../hooks/minutaDIPRES/useEtapa5";
 import { SuccessMinutaDipres } from "../../components/success/minutaDipres";
 import { useAuth } from "../../context/AuthContext";
 
-const SegundaMinuta = () => {
+const SegundaMinuta = () =>
+{
   const { id } = useParams();
   const { competenciaDetails } = useCompetencia(id);
   const { patchArchivoMinuta } = useEtapa5();
-  const [archivoSeleccionado, setArchivoSeleccionado] = useState(null);
-  const [isSubmitSuccessful, setIsSubmitSuccessful] = useState(false);
+  const [ archivoSeleccionado, setArchivoSeleccionado ] = useState(null);
+  const [ isSubmitSuccessful, setIsSubmitSuccessful ] = useState(false);
   const navigate = useNavigate();
   const { userData } = useAuth();
   const minutaEnviada = !!competenciaDetails?.etapa5?.archivo_minuta_etapa5;
   const etapa3omitida = competenciaDetails?.etapa3?.omitida
   const idEtapa = competenciaDetails?.etapa5?.id
-  const [errorMessage, setErrorMessage] = useState("");
+  const [ errorMessage, setErrorMessage ] = useState("");
+  const userObservador = userData?.perfil?.includes("Usuario Observador");
 
-  const handleBackButtonClick = () => {
+  const handleBackButtonClick = () =>
+  {
     navigate(-1);
   };
 
-  const handleVerFormulario = (formularioId) => {
+  const handleVerFormulario = (formularioId) =>
+  {
     navigate(`/home/formulario_gore/${formularioId}/paso_1`);
   };
 
-  const handleFileSelect = (file) => {
+  const handleFileSelect = (file) =>
+  {
     setArchivoSeleccionado(file);
   };
 
-  const handleEnviarMinuta = () => {
-    if (!archivoSeleccionado) {
+  const handleEnviarMinuta = () =>
+  {
+    if (!archivoSeleccionado)
+    {
       setErrorMessage("Por favor, seleccione un archivo antes de enviar la minuta.");
       return;
     }
@@ -63,48 +70,53 @@ const SegundaMinuta = () => {
       </div>
 
       {!isSubmitSuccessful ? (
-      <>
-        {etapa3omitida && (
-        <div className="border-bottom pb-3">
-          <h2 className="text-sans-25 mt-5 mb-4">Formularios sectoriales</h2>
-          {competenciaDetails?.etapa2?.formulario_sectorial ? (
-            Array.isArray(competenciaDetails.etapa2.formulario_sectorial) ? (
-              competenciaDetails.etapa2.formulario_sectorial.map((formulario, index) => (
-                <tr
-                  className={`d-flex justify-content-between p-3 align-items-center ${index % 2 === 0 ? 'neutral-line' : 'white-line'}`}
-                  key={formulario.id}
-                >
-                  <td>{formulario.nombre}</td>
-                  <td className="">
-                    <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
-                      Ver observaciones
-                    </button>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              competenciaDetails.etapa2.formulario_sectorial.detalle_formularios_sectoriales.map((formulario, index) => (
-                <tr
-                  className={`d-flex justify-content-between p-3 align-items-center ${index % 2 === 0 ? 'neutral-line' : 'white-line'}`}
-                  key={formulario.id}
-                >
-                  <td>{formulario.nombre}</td>
-                  <td className="">
-                    <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
-                      Ver observaciones
-                    </button>
-                  </td>
-                </tr>
-              ))
-            )
-          ) : (
-            <p>No hay formularios disponibles.</p>
+        <>
+          {etapa3omitida && (
+            <div className="border-bottom pb-3">
+              <h2 className="text-sans-25 mt-5 mb-4">Formularios sectoriales</h2>
+              {competenciaDetails?.etapa2?.formulario_sectorial ? (
+                Array.isArray(competenciaDetails.etapa2.formulario_sectorial) ? (
+                  competenciaDetails.etapa2.formulario_sectorial.map((formulario, index) => (
+                    <tr
+                      className={`d-flex justify-content-between p-3 align-items-center ${index % 2 === 0 ? 'neutral-line' : 'white-line'}`}
+                      key={formulario.id}
+                    >
+                      <td>{formulario.nombre}</td>
+                      <td className="">
+                      {!userObservador ? (
+                        <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
+                          Ver observaciones
+                        </button>):(<div className="badge-status-finish">Finalizada</div>
+                      )}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  competenciaDetails.etapa2.formulario_sectorial.detalle_formularios_sectoriales.map((formulario, index) => (
+                    <tr
+                      className={`d-flex justify-content-between p-3 align-items-center ${index % 2 === 0 ? 'neutral-line' : 'white-line'}`}
+                      key={formulario.id}
+                    >
+                      <td>{formulario.nombre}</td>
+                      <td className="">
+                      {!userObservador ? (
+                        <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
+                          Ver observaciones
+                        </button>
+                        ):(<div className="badge-status-finish">Finalizada</div>
+                        )}
+                      </td>
+                    </tr>
+                  ))
+                )
+              ) : (
+                <p>No hay formularios disponibles.</p>
+              )}
+            </div>
           )}
-        </div>
-        )}
 
-        <div className="border-bottom pb-3">
-          <h2 className="text-sans-25 mt-5 mb-4">Formularios GORE</h2>
+          <div className="border-bottom pb-3">
+            <h2 className="text-sans-25 mt-5 mb-4">Formularios GORE</h2>
             {competenciaDetails?.etapa4?.formularios_gore ? (
               Array.isArray(competenciaDetails.etapa4.formularios_gore) ? (
                 competenciaDetails.etapa4.formularios_gore.map((formulario, index) => (
@@ -114,9 +126,12 @@ const SegundaMinuta = () => {
                   >
                     <td>{formulario.nombre}</td>
                     <td className="">
-                      <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
-                        Ver observaciones
-                      </button>
+                      {!userObservador ? (
+                        <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
+                          Ver observaciones
+                        </button>) : (
+                        <div className="badge-status-finish">Finalizada</div>
+                      )}
                     </td>
                   </tr>
                 ))
@@ -128,9 +143,12 @@ const SegundaMinuta = () => {
                   >
                     <td>{formulario.nombre}</td>
                     <td className="">
-                      <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
-                        Ver observaciones
-                      </button>
+                      {!userObservador ? (
+                        <button className="btn-secundario-s text-decoration-underline" onClick={() => handleVerFormulario(formulario.id)}>
+                          Ver observaciones
+                        </button>
+                      ) : (<div className="badge-status-finish">Finalizada</div>
+                      )}
                     </td>
                   </tr>
                 ))
@@ -138,73 +156,73 @@ const SegundaMinuta = () => {
             ) : (
               <p>No hay formularios disponibles.</p>
             )}
-        </div>
+          </div>
 
-        <div>
-          {minutaEnviada ? (
-            <h2 className="text-sans-25 mt-5">Minuta DIPRES</h2>
+          <div>
+            {minutaEnviada ? (
+              <h2 className="text-sans-25 mt-5">Minuta DIPRES</h2>
             ) : (
-            <h2 className="text-sans-25 mt-5">Subir minuta (Obligatorio)</h2>
-          )}
-          <h6 className="text-sans-h6 mb-4">Mínimo 1 archivo, peso máximo 20MB, formato PDF</h6>
+              <h2 className="text-sans-25 mt-5">Subir minuta (Obligatorio)</h2>
+            )}
+            <h6 className="text-sans-h6 mb-4">Mínimo 1 archivo, peso máximo 20MB, formato PDF</h6>
 
-          {userData?.perfil === 'DIPRES' && (
-            <>
-              <div className="d-flex justify-content-between py-3 fw-bold">
-                <div className="d-flex mb-2">
-                  <div className="ms-2">#</div>
-                  <div className="ms-5">Documento</div>
+            {userData?.perfil === 'DIPRES' && (
+              <>
+                <div className="d-flex justify-content-between py-3 fw-bold">
+                  <div className="d-flex mb-2">
+                    <div className="ms-2">#</div>
+                    <div className="ms-5">Documento</div>
+                  </div>
+                  <div className="me-5">Acción</div>
                 </div>
-                <div className="me-5">Acción</div>
-              </div>
+                <SubirArchivo
+                  index="1"
+                  handleFileSelect={handleFileSelect}
+                  readOnly={minutaEnviada}
+                  archivoDescargaUrl={competenciaDetails?.etapa5?.archivo_minuta_etapa5}
+                  tituloDocumento={competenciaDetails?.etapa5?.archivo_minuta_etapa5}
+                />
+              </>
+            )}
+
+            {userData?.perfil !== 'DIPRES' && minutaEnviada && (
               <SubirArchivo
                 index="1"
                 handleFileSelect={handleFileSelect}
                 readOnly={minutaEnviada}
                 archivoDescargaUrl={competenciaDetails?.etapa5?.archivo_minuta_etapa5}
-                tituloDocumento={competenciaDetails?.etapa5?.archivo_minuta_etapa5} 
+                tituloDocumento={competenciaDetails?.etapa5?.archivo_minuta_etapa5}
               />
-            </>
-          )}
+            )}
+            {errorMessage && (
+              <p className="text-sans-h6-darkred mt-1 mb-0">
+                {errorMessage}
+              </p>
+            )}
+            {userData?.perfil !== 'DIPRES' && !minutaEnviada && (
+              <p className="text-sans-25 mt-5">Aun no se ha subido Minuta DIPRES.</p>
+            )}
+          </div>
 
-          {userData?.perfil !== 'DIPRES' && minutaEnviada && (
-            <SubirArchivo
-              index="1"
-              handleFileSelect={handleFileSelect}
-              readOnly={minutaEnviada}
-              archivoDescargaUrl={competenciaDetails?.etapa5?.archivo_minuta_etapa5}
-              tituloDocumento={competenciaDetails?.etapa5?.archivo_minuta_etapa5} 
-            />
-          )}
-          {errorMessage && (
-            <p className="text-sans-h6-darkred mt-1 mb-0">
-              {errorMessage}
-            </p>
-          )}
-          {userData?.perfil !== 'DIPRES' && !minutaEnviada && (
-            <p className="text-sans-25 mt-5">Aun no se ha subido Minuta DIPRES.</p>
-          )}
-        </div>
-          
-        <div className="d-flex justify-content-end my-5 me-3">
-          {!minutaEnviada && (
-            <button
-              className="btn-primario-s"
-              disabled={!archivoSeleccionado}
-              onClick={handleEnviarMinuta}
-            >
-              Enviar minuta
-              <i className="material-symbols-rounded me-2">arrow_forward_ios</i>
-            </button>
-          )}
-        </div>
-      </>
+          <div className="d-flex justify-content-end my-5 me-3">
+            {!minutaEnviada && (
+              <button
+                className="btn-primario-s"
+                disabled={!archivoSeleccionado}
+                onClick={handleEnviarMinuta}
+              >
+                Enviar minuta
+                <i className="material-symbols-rounded me-2">arrow_forward_ios</i>
+              </button>
+            )}
+          </div>
+        </>
       ) : (
-        <SuccessMinutaDipres 
-        idCompetencia={competenciaDetails?.id}
+        <SuccessMinutaDipres
+          idCompetencia={competenciaDetails?.id}
         />
       )}
-      
+
     </div>
   );
 };
