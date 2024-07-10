42fa3755b6474dfd15e45996c055bdf724bd07ea
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-07-10 15:16:33 -0400

    responsividad vistas success

--- src/views/success/success_OS_formGore.jsx
+++ src/views/success/success_OS_formGore.jsx
@@ -33,7 +33,7 @@ const SuccessOS_Gore = () => {
   }
 
   return (
-    <div className="container col-11">
+    <div className="container col-11 ps-5 ms-5">
       <h1 className="text-sans-Title mt-5 ms-3">Observaciones SUBDERE</h1>
       <h1 className="text-sans-h1 ms-3">Formularios GORE y DIPRES</h1>
       <h2 className="text-sans-h2-grey pb-5 ms-3">{dataFormGore.competencia_nombre}</h2>
