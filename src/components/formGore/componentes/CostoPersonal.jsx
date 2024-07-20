0ecdad313fa1a372aa42c4c11251cac33ed2f1cd
JaimeHernandezE <jhearquitecto@gmail.com>
2024-07-19 21:03:36 -0400

    FIX: Correcciones Paso3 GORE

--- src/components/formGore/componentes/CostoPersonal.jsx
+++ src/components/formGore/componentes/CostoPersonal.jsx
@@ -1,12 +1,12 @@
 export const CostoPersonal = ({
   title,
-  plantaJustificado,
+  plantaInformado,
   plantaJustificar,
-  contrataJustificado,
+  contrataInformado,
   contrataJustificar,
-  otrasJustificado,
+  otrasInformado,
   otrasJustificar,
-  gastoPersonalJustificado,
+  gastoPersonalInformado,
   gastosPersonalJustificar,
 }) => {
   // Función para formatear los números con separadores de miles
@@ -16,14 +16,14 @@ export const CostoPersonal = ({
 
   // Lista de filas con sus respectivos datos, ya formateados
   const filas = [
-    { nombre: '01 - Personal de Planta', justificado: formatNumber(plantaJustificado), justificar: formatNumber(plantaJustificar) },
-    { nombre: '02 - Personal Contrata', justificado: formatNumber(contrataJustificado), justificar: formatNumber(contrataJustificar) },
-    { nombre: '03 - Otras Remuneraciones', justificado: formatNumber(otrasJustificado), justificar: formatNumber(otrasJustificar) },
-    { nombre: '04 - Otras Gastos en Personal', justificado: formatNumber(gastoPersonalJustificado), justificar: formatNumber(gastosPersonalJustificar) },
+    { nombre: '01 - Personal de Planta', informado: formatNumber(plantaInformado), justificar: formatNumber(plantaJustificar) },
+    { nombre: '02 - Personal Contrata', informado: formatNumber(contrataInformado), justificar: formatNumber(contrataJustificar) },
+    { nombre: '03 - Otras Remuneraciones', informado: formatNumber(otrasInformado), justificar: formatNumber(otrasJustificar) },
+    { nombre: '04 - Otras Gastos en Personal', informado: formatNumber(gastoPersonalInformado), justificar: formatNumber(gastosPersonalJustificar) },
   ];
 
   // Filtrar primero las filas que deben mostrarse
-  const filasVisibles = filas.filter(fila => fila.justificado !== "0" || fila.justificar !== "0");
+  const filasVisibles = filas.filter(fila => fila.informado !== "0" || fila.justificar !== "0");
 
   return (
     <>
@@ -31,18 +31,19 @@ export const CostoPersonal = ({
         <div className="col-12">
           <div className="subrayado col-12">
             <span className="py-2 my-2 align-self-center">
-              <p className="text-sans-p ms-2"> Costos de personal {title} a justificar</p>
+              <p className="text-sans-p ms-2"> Costos de personal {title} a justificar por GORE:</p>
             </span>
           </div>
           <table className="table table-striped align-middle">
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col-5">item</th>
-                <th scope="col-6">
-                  Costo adicional informado por GORE ($M)
+                <th scope="col-4 ">item</th>
+                <th scope="col-4 text-center">
+                  Costo adicional 
+                  <br /> informado por GORE ($M)
                 </th>
-                <th scope="col-4">
+                <th scope="col-3 text-center">
                   Pendiente por justificar
                 </th>
               </tr>
@@ -51,9 +52,9 @@ export const CostoPersonal = ({
               {filasVisibles.map((fila, index) => (
                 <tr key={index}>
                   <th>{index + 1}</th>
-                  <td><span className="text-sans-p-grayc">{fila.nombre}</span></td>
-                  <td className=""><div className="col-6 py-1 valor-fijo mb-2 mt-2">$ {fila.justificado}</div></td>
-                  <td><div className="text-sans-p-bold-darkred">{fila.justificar}</div></td>
+                  <td><span className="text-sans-p-grayc col-4">{fila.nombre}</span></td>
+                  <td className=""><div className="col-6 py-1 valor-fijo mb-2 mt-2 text-center">$ {fila.informado}</div></td>
+                  <td><div className="text-sans-p-bold-darkred text-center col-6">{fila.justificar}</div></td>
                 </tr>
               ))}
             </tbody>
