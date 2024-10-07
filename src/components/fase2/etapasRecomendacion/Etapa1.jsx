fd132bcedb470d344d8b77e960078d499bec3bf2
Rosario Herrera Amunátegui <114428069+RosarioHA@users.noreply.github.com>
2024-10-07 16:05:30 -0300

    agrega subpasos a paso 1 recomendacion post cid

--- src/components/fase2/etapasRecomendacion/Etapa1.jsx
+++ src/components/fase2/etapasRecomendacion/Etapa1.jsx
@@ -1,40 +1,43 @@
 import { Link } from 'react-router-dom';
 
-export const Etapa1Recomendacion = ({ etapa, id }) => {
-  const { competencia_creada, usuarios_vinculados, fecha_ultima_modificacion, ultimo_editor } = etapa;
+export const Etapa1Recomendacion = ({ etapa }) => {
+  const { usuario_notificado, fecha_ultima_modificacion, calcular_tiempo_transcurrido, formulario_recomendacion } = etapa;
+
+  console.log("etapa dentro de etapa1", etapa)
 
   return (
     <div className="my-3">
       <div>
-        {competencia_creada && competencia_creada.length > 0 && competencia_creada.map((competencia, index) => (
-          <div key={index} className="d-flex justify-content-between text-sans-p border-top border-bottom my-3 py-1">
-            <div className="align-self-center">{competencia.nombre}</div>
-            <span className="badge-status-finish">{competencia.accion}</span>
-          </div>
-        ))}
+        <div className="d-flex justify-content-between text-sans-p my-3 py-1">
+          <div className="align-self-center">Para completar esta etapa con éxito deben cumplirse estas condiciones:</div>  
+        </div>
 
-        {/* Manejar usuarios_vinculados que es un objeto individual */}
-        {/* <div className="d-flex justify-content-between text-sans-p border-top border-bottom my-3 py-1">
-            <div className="align-self-center">nombre</div>
-            
-          </div> */}
-        {usuarios_vinculados && (
-          <div className="d-flex justify-content-between text-sans-p border-top border-bottom my-3 py-1">
-            <div className="align-self-center">{usuarios_vinculados.nombre}</div>
-            {usuarios_vinculados.accion === "Editar usuario(s)" ? (
-              <Link to={`/home/editar_competencia/${id}`} className="btn-secundario-s text-decoration-none" id="btn">
-                <span className="material-symbols-outlined me-1">folder</span>
-                <u>{usuarios_vinculados.accion}</u>
-              </Link>
+        <div className="d-flex justify-content-between text-sans-p border-top border-bottom my-3 py-1">
+          <div className="align-self-center">Recomendación de transferencia post-CID</div>
+          <Link to={`/home/`} className="btn-secundario-s text-decoration-none" id="btn">
+            <span className="material-symbols-outlined me-1">insert_drive_file</span>
+            <u>{formulario_recomendacion?.accion}</u>
+          </Link>
+        </div>
+
+        <div className="d-flex justify-content-between text-sans-p border-top border-bottom my-3 py-1">
+          <div className="align-self-center">{usuario_notificado?.nombre}</div>
+          {usuario_notificado?.estado === "finalizado" ? (
+            <span className="badge-status-finish">{usuario_notificado?.estado}</span>  
             ) : (
-              <span className="badge-status-finish">{usuarios_vinculados.accion}</span>
-            )}
-          </div>
-        )}
+            <Link to={`/home/`} className="btn-secundario-s text-decoration-none" id="btn">
+              {/* <span className="material-symbols-outlined me-1">folder</span> */}
+              <u>{usuario_notificado?.accion}</u>
+            </Link>
+          )}
+        </div>
 
-        <div className="d-flex my-3">
-          <div className="text-sans-p me-5">Última modificación por: {ultimo_editor.nombre_completo}</div>
-          <div className="text-sans-p ms-5 ps-5">Fecha última modificación: {fecha_ultima_modificacion}</div>
+        <div className="my-3">
+          <div className="text-sans-p mb-3 mt-4">
+            Tiempo transcurrido:  
+            <b> {calcular_tiempo_transcurrido?.dias} días {calcular_tiempo_transcurrido?.horas} horas {calcular_tiempo_transcurrido?.minutos} minutos </b>
+          </div>
+          <div className="text-sans-p">Fecha última modificación: <b> {fecha_ultima_modificacion}</b></div>
         </div>
       </div>
     </div>
